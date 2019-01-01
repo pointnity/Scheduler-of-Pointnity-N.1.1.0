b40e2e2a0076594439f6d211b77aded7369295fc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-01 16:04:15 +0800

    Update FindThrift.cmake

--- CMakeModules/FindThrift.cmake
+++ CMakeModules/FindThrift.cmake
@@ -5,3 +5,6 @@
 # THRIFT_EXECUTABLE - path to the thrift program
 # THRIFT_INCLUDE_PATH - path to the include file
 # THRIFT_LIBRARY_PATH - path to the library file
+
+FIND_PROGRAM(THRIFT_EXECUTABLE thrift DOC "path to the thrift program")
+MARK_AS_ADVANCED(THRIFT_EXECUTABLE)
