6d5a4555906a2b89ed9372b503b7e3b0c056f9d1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-13 21:04:37 +0800

    Update cancel_hdfs.sh

--- script/cancel_hdfs.sh
+++ script/cancel_hdfs.sh
@@ -8,3 +8,5 @@
 # Description: CP .h .so TO default path(/usr/local/include...)
 # usage: ./setup_hdfs.sh JAVA_HOME HADOOP_DIR
 #######################################
+
+JAVA_HOME=$1 # /usr/lib/java/jdk1.7.0_40/ 
