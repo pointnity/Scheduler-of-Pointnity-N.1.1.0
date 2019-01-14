e1a17769dc015f0abefd9a6a84dd1923b1d7bb85
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-14 16:42:37 +0800

    Update clean_proxy.sh

--- script/clean_proxy.sh
+++ script/clean_proxy.sh
@@ -2,3 +2,4 @@
 
 rm -rf  ../src/proxy/ResourceScheduler/gen-cpp ../src/proxy/JobsManager/gen-cpp ../src/proxy/executor/gen-cpp ../src/proxy/executor/gen-py ../src/proxy/vm_worker/gen-cpp ../src/proxy/vm_worker/gen-py ../src/proxy/JobsManager/gen-py
 
+exit 0
