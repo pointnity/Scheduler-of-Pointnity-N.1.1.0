83a787defeac258764beb5475fe9cbf723183363
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-15 16:27:56 +0800

    Update setup_zk.sh

--- script/setup_zk.sh
+++ script/setup_zk.sh
@@ -32,3 +32,4 @@ cp zoo.cfg server1/zookeeper-3.4.5/conf/zoo.cfg
 echo "dataDir=./server1/data" >> server1/zookeeper-3.4.5/conf/zoo.cfg
 echo "dataLogDir=./server1/dataLog" >> server1/zookeeper-3.4.5/conf/zoo.cfg
 echo "clientPort=2181" >> server1/zookeeper-3.4.5/conf/zoo.cfg
+cp zoo.cfg server2/zookeeper-3.4.5/conf/zoo.cfg
