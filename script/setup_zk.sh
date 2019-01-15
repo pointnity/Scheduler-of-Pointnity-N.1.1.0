349839c3a4d4061525d7ce1c7e98c0b089809ce8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-15 16:25:09 +0800

    Update setup_zk.sh

--- script/setup_zk.sh
+++ script/setup_zk.sh
@@ -14,3 +14,5 @@ cd ../zookeeper
 mkdir -p server1/data server1/dataLog server1/logs
 mkdir -p server2/data server2/dataLog server2/logs
 mkdir -p server3/data server3/dataLog server3/logs
+
+tar -xvzf zookeeper-3.4.5.tar.gz
