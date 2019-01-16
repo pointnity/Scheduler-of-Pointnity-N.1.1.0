4f5ce8fdb018d939e9f66cb6fc1cb2f624006b0f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-16 15:25:12 +0800

    Update zk_control.sh

--- script/zk_control.sh
+++ script/zk_control.sh
@@ -15,3 +15,4 @@ if [ $1 == "start" ]; then
    cd ../zookeeper
    ./server1/zookeeper-3.4.5/bin/zkServer.sh start
    ./server2/zookeeper-3.4.5/bin/zkServer.sh start
+   ./server3/zookeeper-3.4.5/bin/zkServer.sh start
