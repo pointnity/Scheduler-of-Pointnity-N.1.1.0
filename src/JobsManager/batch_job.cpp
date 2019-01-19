732688b9bc71befa11aa8e19b308f8ada9b3faa1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-19 11:43:51 +0800

    Update batch_job.cpp

--- src/JobsManager/batch_job.cpp
+++ src/JobsManager/batch_job.cpp
@@ -11,3 +11,4 @@ bool BatchJob::HaveWaitingTasks() {
     for(list<TaskPtr>::const_iterator it = m_task_list.begin();
         it != m_task_list.end(); ++it) {
         if((*it)->GetTaskState() == TASK_WAITING) {
+            return true;
