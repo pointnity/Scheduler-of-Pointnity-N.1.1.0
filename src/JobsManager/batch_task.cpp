829cad304d4f0de3a15bdde8c1007d5f627fdc78
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-20 13:53:25 +0800

    Update batch_task.cpp

--- src/JobsManager/batch_task.cpp
+++ src/JobsManager/batch_task.cpp
@@ -5,3 +5,4 @@ using clynn::ReadLocker;
 using clynn::WriteLocker;
 
 /* void BatchTask::InitTaskInfo(TaskAdPtr ads, int32_t job_id,
+                             int32_t task_id, int32_t submit_time) {
