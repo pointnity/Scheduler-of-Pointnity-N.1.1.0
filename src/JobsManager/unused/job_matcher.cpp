79830179c77734076df900079dbeeaa42b8a86ba
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-22 16:07:09 +0800

    Update job_matcher.cpp

--- src/JobsManager/unused/job_matcher.cpp
+++ src/JobsManager/unused/job_matcher.cpp
@@ -61,3 +61,4 @@ int32_t JobMatcher::SendRequest(const string& classad, string& match_result) {
         Proxy<ResourceSchedulerClient> proxy = RpcClient<ResourceSchedulerClient>::GetProxy(FLAGS_resource_scheduler_endpoint);
         proxy().MatchMachine(match_result, classad); 
     } catch (TException &tx) {
+        LOG4CPLUS_ERROR(logger, "MatchUnSuccess");
