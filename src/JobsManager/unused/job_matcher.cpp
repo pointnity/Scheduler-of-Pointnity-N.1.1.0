1027bf18e756289a9727e71bbdaeebfb93f5abbe
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-22 16:01:28 +0800

    Update job_matcher.cpp

--- src/JobsManager/unused/job_matcher.cpp
+++ src/JobsManager/unused/job_matcher.cpp
@@ -31,3 +31,4 @@ int32_t JobMatcher::MatchTaskList(list<TaskMatchInfo>& task_match_list) {
              soft_it != it->soft_constraint_list.end(); ++ soft_it) 
         {
             soft_list.push_back(*soft_it);
+        }
