e877219f52ab906790e2903c741b94ef0910fa2c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-24 14:38:40 +0800

    Update config.cpp

--- src/JobsManager/config.cpp
+++ src/JobsManager/config.cpp
@@ -11,3 +11,4 @@
 DEFINE_int32(jobs_manager_port, 9997, "JobsManager rpc port");
 DEFINE_string(resource_scheduler_endpoint, "127.0.0.1:9998", "resource_scheduler endpoint");
 DEFINE_string(groups_xml, "../conf/groups.xml", "groups config file with xml format");
+DEFINE_int32(max_restart_times, 3, "max times of restarting task");
