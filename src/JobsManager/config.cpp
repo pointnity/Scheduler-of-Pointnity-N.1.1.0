c9992bd56e2a7abdefdeb5b2eee810d30e4fc459
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-24 14:19:38 +0800

    Update config.cpp

--- src/JobsManager/config.cpp
+++ src/JobsManager/config.cpp
@@ -9,3 +9,4 @@
 #include "gflags/gflags.h"
 
 DEFINE_int32(jobs_manager_port, 9997, "JobsManager rpc port");
+DEFINE_string(resource_scheduler_endpoint, "127.0.0.1:9998", "resource_scheduler endpoint");
