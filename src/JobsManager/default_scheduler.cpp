fc14239d1aeb0722baf7e8de99a2e9a33b57d579
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-25 14:27:11 +0800

    Update default_scheduler.cpp

--- src/JobsManager/default_scheduler.cpp
+++ src/JobsManager/default_scheduler.cpp
@@ -102,3 +102,4 @@ int32_t DefaultScheduler::ScheduleOneTask(const TaskPtr& task_ptr) {
     vector<string> vec_ip_port;
     StringUtility::Split(result.c_str(), vec_ip_port, ":");
     if (vec_ip_port.size() != 2) {
+        LOG4CPLUS_ERROR(logger, "result is illegal" << result);
