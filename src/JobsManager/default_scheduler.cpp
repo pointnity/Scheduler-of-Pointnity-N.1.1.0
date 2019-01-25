d6169f2a3f98da597b2c06f6c2d54432ba4527ab
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-25 14:17:46 +0800

    Update default_scheduler.cpp

--- src/JobsManager/default_scheduler.cpp
+++ src/JobsManager/default_scheduler.cpp
@@ -42,3 +42,4 @@ int32_t DefaultScheduler::ScheduleOneJob(JobPtr& job_ptr) {
         if (ScheduleOneTask(*it) != 0) {
             has_no_success = true; // has task no success
         } else {
+            has_success = true; // has task success 
