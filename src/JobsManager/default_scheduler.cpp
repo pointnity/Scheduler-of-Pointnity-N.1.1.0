d44d90d4ce59cdbc541f9a14a61071c959e3d392
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-25 14:14:56 +0800

    Update default_scheduler.cpp

--- src/JobsManager/default_scheduler.cpp
+++ src/JobsManager/default_scheduler.cpp
@@ -32,3 +32,4 @@ int32_t DefaultScheduler::ScheduleOneJob(JobPtr& job_ptr) {
 
     list<TaskPtr> task_list = job_ptr->GetTaskList();
     for (list<TaskPtr>::const_iterator it = task_list.begin();
+        it != task_list.end(); ++it)
