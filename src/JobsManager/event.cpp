3da415bc03c99e10a8cf4dd8dda584fc86a3215d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-27 11:25:06 +0800

    Update event.cpp

--- src/JobsManager/event.cpp
+++ src/JobsManager/event.cpp
@@ -57,3 +57,4 @@ bool TaskStartedEvent::Handle() {
     // TaskPtr task_ptr = JobPoolI::Instance()->GetTask(m_job_id, m_task_id);
 
     JobPtr job_ptr = JobPoolI::Instance()->GetJobPtr(m_job_id);
+    if (NULL == job_ptr) {
