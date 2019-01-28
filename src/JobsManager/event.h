7f9b84db15c0995d5fb12338a8d0560196e9ab5f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-28 12:14:29 +0800

    Update event.h

--- src/JobsManager/event.h
+++ src/JobsManager/event.h
@@ -115,3 +115,5 @@ public:
 
 class TaskFinishedEvent : public TaskStateEvent {
 public:
+    TaskFinishedEvent(int32_t job_id, int32_t task_id) : TaskStateEvent(job_id, task_id) {}
+    bool Handle();
