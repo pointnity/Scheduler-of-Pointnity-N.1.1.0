a6ff18d2c431b35f20db5ff7ae110fe3d337f4e4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-29 18:53:09 +0800

    Update group.cpp

--- src/JobsManager/group.cpp
+++ src/JobsManager/group.cpp
@@ -55,3 +55,5 @@ bool Group::AddJobToWaitQueue(const JobPtr& job, bool push_front) {
 }
 
 // Add Job to Wait Queue Bu Queue Num
+bool Group::AddJobToQueueByQueueNum(const JobPtr& job, const JobQueueNum& num, bool push_front){
+    switch (num) {
