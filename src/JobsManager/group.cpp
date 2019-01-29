5342c76bc51c8dd7da167e4f770668fb955bdb13
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-29 18:48:47 +0800

    Update group.cpp

--- src/JobsManager/group.cpp
+++ src/JobsManager/group.cpp
@@ -9,3 +9,6 @@ Description: group: quota & queues
 #include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
 #include <gflags/gflags.h>
+
+#include "proxy/ResourceScheduler/gen-cpp/ResourceScheduler.h"
+#include "common/clynn/rpc.h"
