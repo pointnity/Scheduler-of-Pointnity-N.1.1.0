883e0c5c6550b8f394b7f136437b628ed873e687
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-01-30 11:19:00 +0800

    Update group.h

--- src/JobsManager/group.h
+++ src/JobsManager/group.h
@@ -47,3 +47,4 @@ public:
     // Move navigating job -> queue
     bool MoveNavigatingJobToWaitQueue(bool push_front = false);
     bool MoveNavigatingJobToRunQueue();
+    bool MoveJobToRunQueue(const JobPtr& job_ptr);
