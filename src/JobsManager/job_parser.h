820ec6d6e6354d680872c27b933c4e096ebbf3cd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-10 14:42:40 +0800

    Update job_parser.h

--- src/JobsManager/job_parser.h
+++ src/JobsManager/job_parser.h
@@ -34,3 +34,4 @@ private:
     // After the XML parsing + function is nested, the XML string itself changes, so it is changed to an outer function
     bool ParseJobAd(const string& job_xml);
     bool ParseJobAdHo(const string& job_xml);
+    bool ParseJobAdHe(const string& job_xml);
