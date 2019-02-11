6a8852f408ba88f55f10a3813e410b7992e2d997
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-11 16:42:53 +0800

    Update job_scheduler.cpp

--- src/JobsManager/job_scheduler.cpp
+++ src/JobsManager/job_scheduler.cpp
@@ -30,3 +30,4 @@ int32_t JobScheduler::SendRequest(const string& classad, string& match_result) {
     try {
         Proxy<ResourceSchedulerClient> proxy = RpcClient<ResourceSchedulerClient>::GetProxy(FLAGS_resource_scheduler_endpoint);
         proxy().MatchMachine(match_result, classad); 
+    } catch (TException &tx) {
