008991e4b5b0aa0b78055ba05807e8d350732452
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-11 16:42:24 +0800

    Update job_scheduler.cpp

--- src/JobsManager/job_scheduler.cpp
+++ src/JobsManager/job_scheduler.cpp
@@ -26,3 +26,4 @@ static Logger logger = Logger::getInstance("JobsManager");
 
 int32_t JobScheduler::SendRequest(const string& classad, string& match_result) {
     LOG4CPLUS_INFO(logger, "SendRequest " << classad);
+    Timer timer;
