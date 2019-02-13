86f5dba3f86b1f0cf6799d0773b5e02a2a4263f7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-13 08:52:58 +0800

    Update jobs_manager_service.cpp

--- src/JobsManager/jobs_manager_service.cpp
+++ src/JobsManager/jobs_manager_service.cpp
@@ -5,6 +5,7 @@
  Version:  0.1
  Description: jobs_manager service
 *********************************/
+#include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
 #include "JobsManager/jobs_manager_service.h"
 #include "JobsManager/job_parser.h"
@@ -13,8 +14,3 @@
 #include "JobsManager/dispatcher.h"
 #include "JobsManager/job_pool.h"
 #include "JobsManager/group_pool.h"
-
-using log4cplus::Logger;
-
-static Logger logger = Logger::getInstance("JobsManager");
-
