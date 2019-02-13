f7b46e54d78070936e36c4bdc3b9b87077fd6a5f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-13 08:58:54 +0800

    Update jobs_manager_service.cpp

--- src/JobsManager/jobs_manager_service.cpp
+++ src/JobsManager/jobs_manager_service.cpp
@@ -63,3 +63,7 @@ bool JobsManagerService::TaskFailed(int32_t job_id, int32_t task_id) {
     // Push event into Queue
     EventDispatcherI::Instance()->Dispatch(event->GetType())->PushBack(event);
     return true;
+}
+
+bool JobsManagerService::TaskTimeout(int32_t job_id, int32_t task_id) {
+    if (job_id < 0 || task_id < 0) {
