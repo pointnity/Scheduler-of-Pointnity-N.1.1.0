95213f263478be9841f0fbed58ace48a14e762af
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-13 08:59:56 +0800

    Update jobs_manager_service.cpp

--- src/JobsManager/jobs_manager_service.cpp
+++ src/JobsManager/jobs_manager_service.cpp
@@ -78,3 +78,5 @@ bool JobsManagerService::TaskTimeout(int32_t job_id, int32_t task_id) {
 
 bool JobsManagerService::TaskMissed(int32_t job_id, int32_t task_id) {
     if (job_id < 0 || task_id < 0) {
+        return false;
+    }
