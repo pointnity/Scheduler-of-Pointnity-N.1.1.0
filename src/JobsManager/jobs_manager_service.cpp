34d25fc3e2cfa8b9c20d476a39d524bf16a43625
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-13 08:51:26 +0800

    Update jobs_manager_service.cpp

--- src/JobsManager/jobs_manager_service.cpp
+++ src/JobsManager/jobs_manager_service.cpp
@@ -9,3 +9,5 @@
 #include "JobsManager/jobs_manager_service.h"
 #include "JobsManager/job_parser.h"
 #include "JobsManager/task.h"
+#include "JobsManager/event.h"
+#include "JobsManager/dispatcher.h"
