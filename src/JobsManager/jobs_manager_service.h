0713acbc8655bf78374ec9a2490a1397d81a94dd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-14 08:38:41 +0800

    Update jobs_manager_service.h

--- src/JobsManager/jobs_manager_service.h
+++ src/JobsManager/jobs_manager_service.h
@@ -26,3 +26,4 @@ public:
     bool TaskStarted(int32_t job_id, int32_t task_id);
     bool TaskFinished(int32_t job_id, int32_t task_id);
     bool TaskFailed(int32_t job_id, int32_t task_id);
+    bool TaskMissed(int32_t job_id, int32_t task_id);
