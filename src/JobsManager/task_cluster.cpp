2cec11c9de913fe9b473acc70920d47c09bf3fdb
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-16 08:46:07 +0800

    Update task_cluster.cpp

--- src/JobsManager/task_cluster.cpp
+++ src/JobsManager/task_cluster.cpp
@@ -5,3 +5,5 @@
  Version:  0.1
  Description: task, include ClassAd m_taskad_hard_constraint and list<ClassAd> m_soft_constraint_list
 *********************************/
+
+#include "JobsManager/task.h"
