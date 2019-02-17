4f818fda52c78f9bb9c6012825030e3f78303ed4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-17 10:46:37 +0800

    Update wd_scheduler.cpp

--- src/JobsManager/wd_scheduler.cpp
+++ src/JobsManager/wd_scheduler.cpp
@@ -13,3 +13,4 @@ FileName: JobsManager/wd_scheduler.cpp
 #include "common/clynn/timer.h"
 #include "common/clynn/rpc.h"
 #include "common/classad/classad_complement.h"
+#include "include/classad_attr.h"
