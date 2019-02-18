f6ed49a82268f15085f4153e214880c3759a2ffd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-18 10:04:12 +0800

    Update wd_scheduler.h

--- src/JobsManager/wd_scheduler.h
+++ src/JobsManager/wd_scheduler.h
@@ -15,3 +15,4 @@ class WDScheduler : public JobScheduler{
 public:
     WDScheduler() : JobScheduler(WIDE_DISTRIBUTION) {}
     int32_t ScheduleOneJob(JobPtr& job_ptr);
+    int32_t ScheduleOneTask(const TaskPtr& task_ptr);
