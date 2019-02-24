2922f13a25b1dd827eaeb55ffc5d8c007d50ca22
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-24 14:31:53 +0800

    Update machine.h

--- src/ResourceScheduler/machine.h
+++ src/ResourceScheduler/machine.h
@@ -37,3 +37,5 @@ public:
     //bool CheckAndClearTARMap(const TaskID& task_id);
     bool GetTemproryAllocResourceMap(map<TaskID, AllocResource>& TemproryAllocResourceMap);
     string GetIp();
+    ClassAdPtr GetMachineAd();
+private:
