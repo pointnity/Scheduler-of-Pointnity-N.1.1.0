564c32ec027dda4dcb2186533a21e25d236a1969
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-02-25 11:11:15 +0800

    Update match_maker.cpp

--- src/ResourceScheduler/match_maker.cpp
+++ src/ResourceScheduler/match_maker.cpp
@@ -20,3 +20,4 @@ static Logger logger = Logger::getInstance("ResourceScheduler");
 
 int MatchMaker::Negotiation(const string& job, vector<string>& candidates) {
     LOG4CPLUS_INFO(logger, "receive a job:" << job);
+    candidates.clear();
