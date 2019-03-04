cc24268f45f5b8c7c13d87d6a8a0a579fab1917f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-04 15:11:59 +0800

    Update cond.h

--- src/common/clynn/cond.h
+++ src/common/clynn/cond.h
@@ -55,3 +55,4 @@ namespace clynn {
                     string msg = info;
                     msg += " error: ";
                     msg += strerror(code);
+                    throw runtime_error(msg);
