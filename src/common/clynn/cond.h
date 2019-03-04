7416a8cbc57adcb78d7599e7de921f50d037345f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-04 15:05:28 +0800

    Update cond.h

--- src/common/clynn/cond.h
+++ src/common/clynn/cond.h
@@ -28,3 +28,6 @@ namespace clynn {
             ~Cond() { 
                 pthread_cond_destroy(&m_cond);
             }
+
+            /// send signal to wake up the thread
+            void Signal(){
