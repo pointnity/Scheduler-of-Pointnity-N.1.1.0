040b9de5964ebbdfd3678e1779200926b281e92b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-06 19:19:18 +0800

    Update rpc.h

--- src/common/clynn/rpc.h
+++ src/common/clynn/rpc.h
@@ -97,3 +97,5 @@ public:
                                  serverTransport,
                                  transportFactory,
                                  protocolFactory,
+                                 threadManager);
+        */
