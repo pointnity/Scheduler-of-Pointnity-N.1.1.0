8acceace413b142597588ba698346bcc43d51ba5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-07 14:03:20 +0800

    Update rwlock.h

--- src/common/clynn/rwlock.h
+++ src/common/clynn/rwlock.h
@@ -49,3 +49,7 @@ namespace clynn {
                     msg += " error: ";
                     msg += strerror(code);
                     throw runtime_error(msg);
+                }
+            }
+
+        private:
