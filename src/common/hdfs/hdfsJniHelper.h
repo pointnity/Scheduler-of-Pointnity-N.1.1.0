d9210e40f721b56cd9ee0b462613923ef5dcffcc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-15 11:47:26 +0800

    Update hdfsJniHelper.h

--- src/common/hdfs/hdfsJniHelper.h
+++ src/common/hdfs/hdfsJniHelper.h
@@ -44,3 +44,4 @@ typedef jthrowable Exc;
  * Arguments (the method arguments) must be passed after methSignature
  * RETURNS: -1 on error and 0 on success. If -1 is returned, exc will have 
    a valid exception reference, and the result stored at retval is undefined.
+ */
