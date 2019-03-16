c69d98e4f8cbb28434bc9974c2365cc6b8c33e33
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-16 10:55:04 +0800

    Update hdfsJniHelper.h

--- src/common/hdfs/hdfsJniHelper.h
+++ src/common/hdfs/hdfsJniHelper.h
@@ -76,3 +76,4 @@ char *classNameOfObject(jobject jobj, JNIEnv *env);
 /** getJNIEnv: A helper function to get the JNIEnv* for the given thread.
  * If no JVM exists, then one will be created. JVM command line arguments
  * are obtained from the LIBHDFS_OPTS environment variable.
+ * @param: None.
