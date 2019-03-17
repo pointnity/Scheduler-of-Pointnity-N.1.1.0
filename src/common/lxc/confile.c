d5b35a31f7ac44334ee217e07357cb4463551d4f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-17 12:55:10 +0800

    Update confile.c

--- src/common/lxc/confile.c
+++ src/common/lxc/confile.c
@@ -46,3 +46,4 @@ static int config_network_mtu(const char *, char *, struct lxc_conf *);
 static int config_network_ipv4(const char *, char *, struct lxc_conf *);
 static int config_network_script(const char *, char *, struct lxc_conf *);
 static int config_network_ipv6(const char *, char *, struct lxc_conf *);
+static int config_cap_drop(const char *, char *, struct lxc_conf *);
