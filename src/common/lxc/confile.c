432f5aa90cf640d3618e083a506dc8a7c6c23ffa
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-17 12:58:03 +0800

    Update confile.c

--- src/common/lxc/confile.c
+++ src/common/lxc/confile.c
@@ -65,3 +65,4 @@ static struct config config[] = {
 	{ "lxc.mount",                config_mount                },
 	{ "lxc.rootfs.mount",         config_rootfs_mount         },
 	{ "lxc.rootfs",               config_rootfs               },
+	{ "lxc.pivotdir",             config_pivotdir             },
