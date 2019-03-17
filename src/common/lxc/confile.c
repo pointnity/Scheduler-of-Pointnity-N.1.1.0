be387693b8bedfe8698730fbe93ddc05a9a2a735
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-17 12:54:53 +0800

    Update confile.c

--- src/common/lxc/confile.c
+++ src/common/lxc/confile.c
@@ -44,3 +44,4 @@ static int config_network_hwaddr(const char *, char *, struct lxc_conf *);
 static int config_network_vlan_id(const char *, char *, struct lxc_conf *);
 static int config_network_mtu(const char *, char *, struct lxc_conf *);
 static int config_network_ipv4(const char *, char *, struct lxc_conf *);
+static int config_network_script(const char *, char *, struct lxc_conf *);
