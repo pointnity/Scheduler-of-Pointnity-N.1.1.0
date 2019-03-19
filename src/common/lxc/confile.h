5a525723edfb5bce9e8cad24ef2720da727594d2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-19 10:17:56 +0800

    Update confile.h

--- src/common/lxc/confile.h
+++ src/common/lxc/confile.h
@@ -10,3 +10,5 @@ struct lxc_list;
 
 extern int lxc_config_read(const char *file, struct lxc_conf *conf);
 extern int lxc_config_readline(char *buffer, struct lxc_conf *conf);
+
+extern int lxc_config_define_add(struct lxc_list *defines, char* arg);
