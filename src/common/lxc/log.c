8beb1417357a7754851114e1a14d6f29364919e6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-19 10:30:04 +0800

    Update log.c

--- src/common/lxc/log.c
+++ src/common/lxc/log.c
@@ -41,3 +41,4 @@ static int log_append_stderr(const struct lxc_log_appender *appender,
 static int log_append_logfile(const struct lxc_log_appender *appender,
 			      struct lxc_log_event *event)
 {
+	char buffer[LXC_LOG_BUFFER_SIZE];
