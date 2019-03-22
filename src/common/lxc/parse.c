a4fd178b4ba77b00511d1a2c67b9e671011357ae
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-22 14:56:06 +0800

    Update parse.c

--- src/common/lxc/parse.c
+++ src/common/lxc/parse.c
@@ -37,3 +37,4 @@ int lxc_dir_for_each(const char *name, const char *directory,
 	while (n--) {
 		if (!ret &&
 		    callback(name, directory, namelist[n]->d_name, data)) {
+			ERROR("callback failed");
