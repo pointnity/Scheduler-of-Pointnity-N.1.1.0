42b7339b170249ebb7e51a1a5ff04dfa5e9f5111
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-23 17:05:35 +0800

    Update parse.h

--- src/common/lxc/parse.h
+++ src/common/lxc/parse.h
@@ -14,3 +14,5 @@ extern int lxc_dir_for_each(const char *name, const char *directory,
 
 extern int lxc_file_for_each_line(const char *file, lxc_file_cb callback,
 				  void* data);
+
+extern int lxc_char_left_gc(char *buffer, size_t len);
