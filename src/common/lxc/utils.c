3373766b2af0ffb43f438bb2aa4c6294d2bdbfe2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-23 17:26:57 +0800

    Update utils.c

--- src/common/lxc/utils.c
+++ src/common/lxc/utils.c
@@ -30,3 +30,25 @@ int lxc_copy_file(const char *srcfile, const char *dstfile)
 	if (dstfd < 0) {
 		SYSERROR("failed to creat '%s'", dstfile);
 		goto out;
+	}
+
+	srcfd = open(srcfile, O_RDONLY);
+	if (srcfd < 0) {
+		SYSERROR("failed to open '%s'", srcfile);
+		goto err;
+	}
+
+	if (fstat(srcfd, &stat)) {
+		SYSERROR("failed to stat '%s'", srcfile);
+		goto err;
+	}
+
+	if (!stat.st_size) {
+		INFO("copy '%s' which is an empty file", srcfile);
+		ret = 0;
+		goto out_close;
+	}
+
+	if (lseek(dstfd, stat.st_size - 1, SEEK_SET) < 0) {
+		SYSERROR("failed to seek dest file '%s'", dstfile);
+		goto err;
