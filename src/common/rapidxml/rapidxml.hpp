87d931dce7ba0bf7df174bd58781ba7e20f348d0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-25 12:00:41 +0800

    Update rapidxml.hpp

--- src/common/rapidxml/rapidxml.hpp
+++ src/common/rapidxml/rapidxml.hpp
@@ -24,3 +24,4 @@ namespace rapidxml
 {
     //! When exceptions are disabled by defining RAPIDXML_NO_EXCEPTIONS, 
     //! this function is called to notify user about the error.
+    //! It must be defined by the user.
