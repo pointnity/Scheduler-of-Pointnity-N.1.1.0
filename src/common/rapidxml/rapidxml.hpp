274e686ff8f4c094a6e0e505163fda0202ca4683
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-26 11:32:27 +0800

    Update rapidxml.hpp

--- src/common/rapidxml/rapidxml.hpp
+++ src/common/rapidxml/rapidxml.hpp
@@ -137,3 +137,4 @@ namespace rapidxml
     {
         node_document,      //!< A document node. Name and value are empty.
         node_element,       //!< An element node. Name contains element name. Value contains text of first data node.
+        node_data,          //!< A data node. Name is empty. Value contains data text.
