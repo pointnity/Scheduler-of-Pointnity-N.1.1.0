f8c94ff99e80397dbd418516d8645e19eb17b0b7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-26 11:27:50 +0800

    Update rapidxml.hpp

--- src/common/rapidxml/rapidxml.hpp
+++ src/common/rapidxml/rapidxml.hpp
@@ -106,3 +106,4 @@ namespace rapidxml
     // Size of static memory block of memory_pool.
     // Define RAPIDXML_STATIC_POOL_SIZE before including rapidxml.hpp if you want to override the default value.
     // No dynamic memory allocations are performed by memory_pool until static memory is exhausted.
+    #define RAPIDXML_STATIC_POOL_SIZE (64 * 1024)
