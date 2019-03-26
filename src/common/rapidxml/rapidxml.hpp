6f4f27338a67247953bd05e8800eee14eabce764
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-26 11:28:31 +0800

    Update rapidxml.hpp

--- src/common/rapidxml/rapidxml.hpp
+++ src/common/rapidxml/rapidxml.hpp
@@ -111,3 +111,4 @@ namespace rapidxml
 
 #ifndef RAPIDXML_DYNAMIC_POOL_SIZE
     // Size of dynamic memory block of memory_pool.
+    // Define RAPIDXML_DYNAMIC_POOL_SIZE before including rapidxml.hpp if you want to override the default value.
