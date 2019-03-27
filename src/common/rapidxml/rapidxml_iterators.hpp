dedb3ea3be7fbf9e0bea7fd3a048d6d716fbc1d2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-27 16:48:08 +0800

    Update rapidxml_iterators.hpp

--- src/common/rapidxml/rapidxml_iterators.hpp
+++ src/common/rapidxml/rapidxml_iterators.hpp
@@ -19,3 +19,5 @@ namespace rapidxml
         typedef typename xml_node<Ch> *pointer;
         typedef std::ptrdiff_t difference_type;
         typedef std::bidirectional_iterator_tag iterator_category;
+        
+        node_iterator()
