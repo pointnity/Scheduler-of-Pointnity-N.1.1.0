db494735777f0d6277306feeb563692821c0b566
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-29 13:31:28 +0800

    Update rapidxml_utils.hpp

--- src/common/rapidxml/rapidxml_utils.hpp
+++ src/common/rapidxml/rapidxml_utils.hpp
@@ -2,3 +2,5 @@
 #define RAPIDXML_UTILS_HPP_INCLUDED
 //! \file rapidxml_utils.hpp This file contains high-level rapidxml utilities that can be useful
 //! in certain simple scenarios. They should probably not be used if maximizing performance is the main objective.
+
+#include "rapidxml.hpp"
