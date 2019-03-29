c72a04c1acd7d123cb8a0ec4aed64aa0eb710278
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-03-29 13:38:53 +0800

    Update rapidxml_utils.hpp

--- src/common/rapidxml/rapidxml_utils.hpp
+++ src/common/rapidxml/rapidxml_utils.hpp
@@ -51,3 +51,4 @@ namespace rapidxml
             // Load data and add terminating 0
             stream.unsetf(ios::skipws);
             m_data.assign(istreambuf_iterator<Ch>(stream), istreambuf_iterator<Ch>());
+            if (stream.fail() || stream.bad())
