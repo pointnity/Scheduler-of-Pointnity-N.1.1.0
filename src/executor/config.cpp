bbf792348cdff588c86426c6eac77db68d13b54a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-05 09:42:06 +0800

    Update config.cpp

--- src/executor/config.cpp
+++ src/executor/config.cpp
@@ -21,3 +21,4 @@ DEFINE_string(lxc_template, "/var/lib/lynn/lxc/", "default lxc conf template");
 DEFINE_string(hdfs_host, "localhost", "hdfs local host name, uname -n");
 DEFINE_int32(hdfs_port, 9000, "hdfs default port");
 DEFINE_bool(lxc_create, true, "include lxc-create -n 'lxc-name'");
+DEFINE_bool(lxc_relative_path, true, "lxc relative path");
