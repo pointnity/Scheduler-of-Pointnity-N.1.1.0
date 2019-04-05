1a4df483100718d11277844435f2b410f6301699
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-05 09:40:57 +0800

    Update config.cpp

--- src/executor/config.cpp
+++ src/executor/config.cpp
@@ -12,3 +12,4 @@ DEFINE_string(jobs_manager_endpoint, "127.0.0.1:9999", "JobsManager endpoint");
 DEFINE_int32(hb_interval, 15, "heartbeat interval");
 DEFINE_int32(vm_hb_interval, 15, "vm heartbeat interval");
 DEFINE_string(interface, "br0", "network interface, communicate with master");
+DEFINE_string(if_bridge, "br0", "network interface, communicate with vms");
