66a0fad314dde1e54b7190aa80710a26246ab2e5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-05 09:40:24 +0800

    Update config.cpp

--- src/executor/config.cpp
+++ src/executor/config.cpp
@@ -8,3 +8,4 @@
 
 DEFINE_int32(port, 9997, "executor port");
 DEFINE_string(resource_scheduler_endpoint, "127.0.0.1:9998", "ResouceScheduler endpoint");
+DEFINE_string(jobs_manager_endpoint, "127.0.0.1:9999", "JobsManager endpoint");
