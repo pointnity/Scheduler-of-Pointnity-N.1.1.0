69820d68ee46e4a7635eb2da0d7e0ca818928cce
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-07 12:28:44 +0800

    Update event.h

--- src/executor/event.h
+++ src/executor/event.h
@@ -47,3 +47,4 @@ private:
 class ImageEvent : public Event {
 public:
     ImageEvent(const string& user, const string& name, const int32_t size) : Event(EventType::IMAGE_EVENT),
+             m_name(name), m_user(user), m_size(size) {}
