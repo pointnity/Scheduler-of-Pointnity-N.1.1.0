230697d723854033561807ccd3b41fc5ac030e78
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-08 10:20:22 +0800

    Update executor_main.cpp

--- src/executor/executor_main.cpp
+++ src/executor/executor_main.cpp
@@ -106,3 +106,93 @@ int ExecutorEntity(int argc, char **argv) {
     if (!ResourceMgrI::Instance()->Init()) {
         LOG4CPLUS_ERROR(logger, "Failed to initialize resource manager.");
         return EXIT_FAILURE;
+    }
+
+    // init ip pool
+    if (!IPPoolI::Instance()->Init()) {
+        LOG4CPLUS_ERROR(logger, "Failed to initialize IPPool.");
+        return EXIT_FAILURE;
+    }
+    // test TODO
+    /*
+    IPPoolI::Instance()->PrintAll();
+    string ip =  IPPoolI::Instance()->GetAvailIp();
+    IPPoolI::Instance()->PrintAll();
+    if (!IPPoolI::Instance()->ReleaseIp(ip)) {
+         LOG4CPLUS_ERROR(logger, "Failed to relase ip in IPPool.");
+         return -1;
+    } 
+    IPPoolI::Instance()->PrintAll();
+    */
+
+    // work thread
+    //pthread_t task_t;
+    //pthread_create(&task_t, NULL, TaskProcessor, NULL);
+
+    pthread_t vm_t;
+    pthread_create(&vm_t, NULL, VMProcessor, NULL);
+
+    // TODO
+    pthread_t hb_t;
+    pthread_create(&hb_t, NULL, HeartbeatProcessor, NULL);
+
+    /* event dispatcher */
+    //heartbeat event
+    Handler* heartbeat_event_handler = new Handler;
+    heartbeat_event_handler->Start();
+    EventDispatcherI::Instance()->Register(EventType::HEARTBEAT_EVENT, heartbeat_event_handler);
+
+    // task action event
+    Handler* task_action_event_handler = new Handler;
+    task_action_event_handler->Start();
+    EventDispatcherI::Instance()->Register(EventType::TASK_ACTION_EVENT, task_action_event_handler);
+
+    // task state event
+    Handler* task_state_event_handler = new Handler;
+    task_state_event_handler->Start();
+    EventDispatcherI::Instance()->Register(EventType::TASK_STATE_EVENT, task_state_event_handler);
+
+    //image event
+    Handler* image_event_handler = new Handler;
+    image_event_handler->Start();
+    EventDispatcherI::Instance()->Register(EventType::IMAGE_EVENT, image_event_handler);
+
+    //executor exit event
+    Handler* executor_event_handler = new Handler;
+    executor_event_handler->Start();
+    EventDispatcherI::Instance()->Register(EventType::EXIT_EXECUTOR_EVENT, executor_event_handler);
+
+    cout << "Executor is OK." << endl;
+
+    // Listen for service 
+    RpcServer<ExecutorService, ExecutorProcessor>::Listen(FLAGS_port);
+    //int port = 9999; 
+    //Rpc<ExecutorService, ExecutorProcessor>::Listen(port);
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+    // is root?
+    if (geteuid() != 0) {
+        fprintf(stderr, "Executor: must be run as root, or sudo run it.\n");
+        return EXIT_FAILURE;
+    }
+
+    ExecutorEntity(argc, argv);
+    // monitor ExecutorEntity
+    /* while(true) {
+        int32_t status;
+        int32_t pid = fork();
+        if (pid != 0) {
+            // parent process, start executorEntity when ExecutorEntity fail
+            if (waitpid(pid, &status, 0) > 0) {
+                continue;
+            }
+        } else {
+            // child process
+            ExecutorEntity(argc, argv);
+        }
+    }*/
+    return EXIT_SUCCESS;
+}
