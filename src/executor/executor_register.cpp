49d94b40696c74f01e9e51d5a071ea969d524b43
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-09 14:15:52 +0800

    Update executor_register.cpp

--- src/executor/executor_register.cpp
+++ src/executor/executor_register.cpp
@@ -31,3 +31,4 @@ bool ExecutorRegister::RegistMachine() {
     try {
         Proxy<ResourceSchedulerClient> proxy = RpcClient<ResourceSchedulerClient>::GetProxy(FLAGS_resource_scheduler_endpoint);
         if(proxy().RegistMachine(ip, FLAGS_lynn_version) == -1) {
+	    LOG4CPLUS_ERROR(logger, "Failed to regist machine , lynn version  error");
