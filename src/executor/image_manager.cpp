8938bf743f148ce7a2a173a19787be29f87c7154
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-12 13:44:14 +0800

    Update image_manager.cpp

--- src/executor/image_manager.cpp
+++ src/executor/image_manager.cpp
@@ -34,3 +34,5 @@ bool ImageManager::ExecutorUpdateImage(const string& image_user, const string& i
     //get image path
     const string image_hdfs_path = "/lynn/" + image_user + "/img/" + image_name;
     const string image_local_path = FLAGS_libvirt_dir + image_name;
+
+    if(FLAGS_debug) {
