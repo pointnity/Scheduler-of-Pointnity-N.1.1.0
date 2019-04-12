6423861a40bfbe6a21a8dcbe7f5fa97ecdc67a83
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-12 13:46:00 +0800

    Update image_manager.cpp

--- src/executor/image_manager.cpp
+++ src/executor/image_manager.cpp
@@ -37,3 +37,5 @@ bool ImageManager::ExecutorUpdateImage(const string& image_user, const string& i
 
     if(FLAGS_debug) {
         LOG4CPLUS_INFO(logger, "image update from hdfs start");
+     }
+    //open hdfs
