61312ad434cdedf4728a9995ecedf6891cb0f158
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-04-21 15:36:21 +0800

    Update resource_manager.cpp

--- src/executor/resource_manager.cpp
+++ src/executor/resource_manager.cpp
@@ -167,3 +167,7 @@ void ResourceManager::GenerateHb(string& machine_str_ad, vector<string>& task_li
     
 
     // vm list
+    hb_m_info.hb_vminfo_list = VMPoolI::Instance()->GetAllHbVMInfo();
+    vector<HbVMInfo> vm_list = hb_m_info.hb_vminfo_list;
+    for (vector<HbVMInfo>::iterator it = vm_list.begin();
+         it != vm_list.end(); ++it) {
