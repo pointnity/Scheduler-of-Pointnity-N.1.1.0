0448990ad2506862faca0199e0f7d36d18df975e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-05-01 13:56:56 +0800

    Update service.cpp

--- src/executor/service.cpp
+++ src/executor/service.cpp
@@ -71,3 +71,5 @@ bool ExecutorService::KillTask(const int32_t job_id, const int32_t task_id) {
     id.job_id = job_id;
     id.task_id = task_id;
     // new KillActionEvent
+    EventPtr event(new KillActionEvent(id));
+    // Push event into Queue
