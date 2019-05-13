7179a70f2dfacbc22fd6607b66bac13fc326fadd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-05-13 18:19:13 +0800

    Update task_entity.cpp

--- src/executor/task_entity.cpp
+++ src/executor/task_entity.cpp
@@ -64,3 +64,5 @@ bool TaskEntity::IsLegal() {
     }
     m_info.type = VMType::type(type);
     // printf("VMType: %d\n", m_info.type);
+
+    if (!ad_ptr->EvaluateAttrBool(ATTR_IS_RUN, m_info.is_run)) {
