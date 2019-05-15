4ca7f47dd46a8e5dc82dd40af74b4178ae461435
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-05-15 13:37:15 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -71,3 +71,7 @@ bool TaskEntityPool::FindToDo(const TaskID id, TaskFunc func) {
     }
     
     return false;    
+}
+
+// travesal m_task_map by order, start the first waiting taskEntity
+// other taskentitys will be started periodically
