e8ea344f9c3c3604bbe9cd9bc7f4f3c9e79daf34
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-05-15 13:17:55 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -30,3 +30,6 @@ void TaskEntityPool::PrintAll() {
         printf("taskentity job_id:%d, task_id:%d\n", (it->first).job_id, (it->first).task_id);
     }    
     printf("**************************************\n");
+}
+
+bool TaskEntityPool::Find(const TaskPtr& ptr) {
