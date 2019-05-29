9af89daddb545f1788daecb4c869434db4c6de84
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-05-29 10:40:14 +0800

    Update checker.cpp

--- src/executor_monitor/checker.cpp
+++ src/executor_monitor/checker.cpp
@@ -41,3 +41,7 @@ bool Checker::CheckVersion() {
     if (getcwd(cur_dir, sizeof(cur_dir)-1) == NULL) {
          LOG4CPLUS_ERROR(logger, "Failed to get current dir.");
          return false;
+    }
+    string executor_dir(cur_dir);
+
+    try {
