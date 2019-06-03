01b1c1d33db44a507af5da93d6609cd0661a0f3b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-03 10:46:21 +0800

    Update main.cpp

--- src/executor_monitor/main.cpp
+++ src/executor_monitor/main.cpp
@@ -58,3 +58,4 @@ int ExecutorMonitorEntity(int argc, char **argv) {
     Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("executor_monitor"));
     logger.addAppender(append);
     logger.setLogLevel(log4cplus::DEBUG_LOG_LEVEL);
+    LOG4CPLUS_DEBUG(logger, "This is the FIRST debug message");
