6f54edd64517487a7fc2e4b3b7e87687cb6c8527
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-09 20:40:04 +0800

    Update classad_attr.h

--- src/include/classad_attr.h
+++ src/include/classad_attr.h
@@ -42,3 +42,4 @@ static const string ATTR_APP_OUT_DIR = "OUT_DIR";
 // -- inside vm, windows or linux
 static const string ATTR_INSTALL_DIR = "INSTALL_DIR";
 static const string ATTR_EXE_PATH = "EXE_PATH";
+static const string ATTR_ARGUMENT = "ATTR_ARGUMENT";
