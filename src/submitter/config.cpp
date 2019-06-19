14e420852dd7e0e29f75939b150bf23102dddc5d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-19 09:57:35 +0800

    Update config.cpp

--- src/submitter/config.cpp
+++ src/submitter/config.cpp
@@ -10,3 +10,4 @@ DEFINE_string(JobsManager_endpoint, "127.0.0.1:9999", "JobsManager endpoint");
 DEFINE_string(xml_path, "", "xml path");
 DEFINE_int32(hdfs_port, 9000, "hdfs port");
 DEFINE_string(hdfs_host, "localhost", "hdfs host");
+DEFINE_bool(first_create_image, true, "image is first create or not");
