74d098a1926302b5b90b80116007760e79905352
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-20 16:11:33 +0800

    Update hdfs_wrapper.cpp

--- src/submitter/hdfs_wrapper.cpp
+++ src/submitter/hdfs_wrapper.cpp
@@ -41,3 +41,4 @@ bool HDFSWrapper::Init() {
     string local_path2 = "/home/wm/b.cpp";
     string h_path2 = "/user/root/b.cpp";
     if (!CopyFromLocalFile(local_path2, h_path2)) {
+        LOG4CPLUS_ERROR(logger, "Failed to copy file");
