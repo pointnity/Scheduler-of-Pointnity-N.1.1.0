cd829387a66ba2c6ee5f9733ed6a93114c1a2a77
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-22 11:53:36 +0800

    Update image.cpp

--- src/submitter/image.cpp
+++ src/submitter/image.cpp
@@ -75,3 +75,4 @@ bool Image::CreateImage(const string& xml_path){
            Proxy<JobsManagerClient> proxy = Rpc<JobsManagerClient, JobsManagerClient>::GetProxy(FLAGS_JobsManager_endpoint);
            if(false == proxy().UpdateImage(m_user, m_name, m_size, m_is_update_all)){
                LOG4CPLUS_ERROR(logger, "update image faile, master error");
+	       fprintf(stderr, "update image faile, rpc error");  
