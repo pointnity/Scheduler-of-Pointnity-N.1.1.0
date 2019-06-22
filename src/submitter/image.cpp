d571b9e1eed2e3d255c2dfc9d7368a127bd0af05
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-22 11:52:07 +0800

    Update image.cpp

--- src/submitter/image.cpp
+++ src/submitter/image.cpp
@@ -65,3 +65,4 @@ bool Image::CreateImage(const string& xml_path){
             LOG4CPLUS_ERROR(logger, "send image file to hdfs  error");
             fprintf(stderr, "send image file to hdfs  error\n");
 	    return false;
+        }
