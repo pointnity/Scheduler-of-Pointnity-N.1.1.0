88a6ba921dc08a26244b8b041130204168b99b7c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-24 10:26:30 +0800

    Update job.cpp

--- src/submitter/job.cpp
+++ src/submitter/job.cpp
@@ -49,3 +49,4 @@ bool Job::CreateJob(const string& xml_path){
 
 bool Job::QueryJob(const int32_t job_id){
     std::cout<< "query job id:"<<job_id<<std::endl;
+    vector<TaskStateInfo> task_state_info_list;
