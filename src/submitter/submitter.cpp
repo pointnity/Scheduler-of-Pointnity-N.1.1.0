9545b086f54b021fd87d104c5fc3d5647f1ce8ed
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-26 14:36:19 +0800

    Update submitter.cpp

--- src/submitter/submitter.cpp
+++ src/submitter/submitter.cpp
@@ -46,3 +46,4 @@ DECLARE_int32(image_id);
 int main(int argc, char **argv) {
     //is root ?
     if (geteuid() != 0) {
+        fprintf(stderr, "submittor: must be run as root, or sudo run it.\n");
