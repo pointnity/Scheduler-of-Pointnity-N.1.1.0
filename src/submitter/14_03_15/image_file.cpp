bc8585dd529fae848d89fc5fa431c4c117df4481
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-06-29 16:26:08 +0800

    Update image_file.cpp

--- src/submitter/14_03_15/image_file.cpp
+++ src/submitter/14_03_15/image_file.cpp
@@ -42,3 +42,4 @@ bool ImageFile::DeleteImageFile(const string& image_name, const string& image_us
     //delete image on hdfs
     const string image_hdfs_path = image_user + "/img/" + image_name;
     if(false == HDFSMgrI::Instance()->DeleteHDFSFile(image_hdfs_path)){
+        LOG4CPLUS_ERROR(logger, "delete imge to hdfs error");
