03285eb578d81d53263a259a1e2cd73d0c903a87
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-10 13:56:24 +0800

    Update classad_match.cpp

--- src/test/classad_match.cpp
+++ src/test/classad_match.cpp
@@ -22,3 +22,5 @@ int main(int argc, char ** argv) {
     c1.InsertAttr(ATTR_AVAIL_VCPU, 4);
     c1.InsertAttr(ATTR_ALL_MEMORY, 6 * 1024); 
     c1.InsertAttr(ATTR_AVAIL_MEMORY, 6 * 1024); 
+
+    ad.alternateScope = &c1;
