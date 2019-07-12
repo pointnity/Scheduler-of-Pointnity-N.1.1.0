145d59cf0e447926c20cb24b8808d756337b062a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-12 09:29:53 +0800

    Update classad_simple.cpp

--- src/test/classad_simple.cpp
+++ src/test/classad_simple.cpp
@@ -17,3 +17,4 @@ int32_t main(int argc, char* argv[]){
     string         str = "[ ATTR_JOB_ID = 18; ATTR_TASK_ID = 1; ATTR_VMHB_CPU = 0.000000; ATTR_VMHB_MEMORY = 0.456332; ATTR_VMHB_BYTES_IN = 170008; ATTR_VMHB_BYTES_OUT = 27905; ATTR_VMHB_STATE = \"APP_RUNNING\" ]";
     ClassAd        *classad;
     ClassAdParser  parser;
+    classad = parser.ParseClassAd(classad_string, true);
