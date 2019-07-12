d9f8929fac3ec0c5ff7922a401dc4e33f8489fc1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-13 07:41:56 +0800

    Update log4cplus.cpp

--- src/test/log4cplus.cpp
+++ src/test/log4cplus.cpp
@@ -19,3 +19,4 @@ LOG4CPLUS_WARN(logger, "This is a bool: " << true);
 LOG4CPLUS_WARN(logger, "This is a char: " << 'x');
 LOG4CPLUS_WARN(logger, "This is a short: " << (short)-100);
 LOG4CPLUS_WARN(logger, "This is a unsigned short: " << (unsigned short)100);
+LOG4CPLUS_WARN(logger, "This is a int: " << (int)1000);
