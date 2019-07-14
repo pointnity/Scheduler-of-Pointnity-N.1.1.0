0eefe7da83319431523a14ff0be24c96de948c34
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-14 11:19:03 +0800

    Update log4cplus_simple.cpp

--- src/test/log4cplus_simple.cpp
+++ src/test/log4cplus_simple.cpp
@@ -22,3 +22,4 @@ LOG4CPLUS_WARN(logger, "This is a unsigned short: " << (unsigned short)100);
 LOG4CPLUS_WARN(logger, "This is a int: " << (int)1000);
 LOG4CPLUS_WARN(logger, "This is a unsigned int: " << (unsigned int)1000);
 LOG4CPLUS_WARN(logger, "This is a long(hex): " << hex << (long)100000000);
+LOG4CPLUS_WARN(logger, "This is a unsigned long: "
