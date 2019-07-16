cc1bd1b33ebc3e034ff2dde0bb78968142083a9e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-16 18:06:41 +0800

    Update test.cpp

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -18,3 +18,4 @@ int32_t main(int argc, char* argv[]){
     string         str = "[ ATTR_JOB_ID = 18; ATTR_TASK_ID = 1; ATTR_VMHB_CPU = 0.000000; ATTR_VMHB_MEMORY = 0.456332; ATTR_VMHB_BYTES_IN = 170008; ATTR_VMHB_BYTES_OUT = 27905; ATTR_VMHB_STATE = \"APP_RUNNING\" ]";
     //string	str = "[ ATTR_JOB_ID = 2 ]";
     ClassAd        *classad;
+    ClassAdParser  parser;
