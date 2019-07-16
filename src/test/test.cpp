b227d127b80401c6d749605754e7727f67132b56
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-16 18:10:02 +0800

    Update test.cpp

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -27,3 +27,4 @@ int32_t main(int argc, char* argv[]){
     ClassAdPtr ptr2(new ClassAd(*ptr.get()));
     printf("%d\n", ptr.use_count()); 
     // if(!ptr->EvaluateAttrInt(ATTR_JOB_ID, id.job_id))
+    if(!ptr->EvaluateAttrString("ATTR_VMHB_STATE",state)) { 
