43ddb940ca296e64a37ed70a3e0cb21306dc005f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-24 19:16:14 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -45,3 +45,4 @@ TEST(TestZookeeperCommon, TestCreateNode) {
 TEST(TestZookeeperCommon, TestCreateNodeWithValue) {
     ZookeeperCommon my_zk;
     std::string value;
+    int rt = my_zk.Init("xaec-test_for_zk_common");
