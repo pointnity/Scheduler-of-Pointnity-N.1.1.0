6c73535fe0824b6e11946fc6be8391449416d710
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-26 10:50:48 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -115,3 +115,4 @@ TEST(TestZookeeperCommon, TestDeleteNodeForce) {
         EXPECT_EQ(rt, 0);
         rt = my_zk.DeleteNodeForce("/zk/xaec/tborg/xaec-test_for_zk_common/tns/test_del");
         EXPECT_EQ(rt, -1);
+        rt = my_zk.DeleteNode("/zk/xaec/tborg/xaec-test_for_zk_common/tns/test_del/job_1/test");
