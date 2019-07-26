289e29afe250d30e6841cd903731ae1c2330cf23
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-26 10:48:50 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -106,3 +106,4 @@ TEST(TestZookeeperCommon, TestDeleteNodeForce) {
         LOG(ERROR) << "connect zk failed.";
     } else {
         EXPECT_EQ(rt, 0);
+        rt = my_zk.CreateNodeForce("/zk/xaec/tborg/xaec-test_for_zk_common/tns/test_del/job_0");
