b843ab3770aadf9584cc997d85e6b950470fc33f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-26 10:52:02 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -122,3 +122,4 @@ TEST(TestZookeeperCommon, TestDeleteNodeForce) {
         EXPECT_EQ(rt, 0);
 
         rt = my_zk.CheckPathExist("/zk/xaec/tborg/xaec-test_for_zk_common/tns/test_del");
+        EXPECT_EQ(rt, -1);
