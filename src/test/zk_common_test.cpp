33986b08f45698bfec8314dc3af458b7958ea005
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-27 20:17:53 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -225,3 +225,5 @@ TEST(TestZookeeperCommon, TestWGetValueOfNode) {
     std::string value;
     int rt = my_zk.Init("xaec-test_for_zk_common");
     if (rt < 0) {
+        LOG(ERROR) << "connect zk failed.";
+    } else {
