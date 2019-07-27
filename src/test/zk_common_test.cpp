dd77e895d1ede16c7d422dacd1a79626d1d1de30
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-27 20:17:40 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -223,3 +223,5 @@ TEST(TestZookeeperCommon, TestWCheckPathExist) {
 TEST(TestZookeeperCommon, TestWGetValueOfNode) {
     ZookeeperCommon my_zk;
     std::string value;
+    int rt = my_zk.Init("xaec-test_for_zk_common");
+    if (rt < 0) {
