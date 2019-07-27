d7a846537aa9b0548b525f70f13295cd68f94911
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-27 20:12:16 +0800

    Update zk_common_test.cpp

--- src/test/zk_common_test.cpp
+++ src/test/zk_common_test.cpp
@@ -165,3 +165,6 @@ TEST(TestZookeeperCommon, TestCreateNodeForce) {
         rt = my_zk.CheckPathExist("/zk/xaec/tborg/xaec-test_for_zk_common/tns/mavisluo"
                                   "/second_node");
         EXPECT_EQ(rt, 0);
+        my_zk.DeleteNode("/zk/xaec/tborg/xaec-test_for_zk_common/tns/mavisluo/second_node");
+        my_zk.DeleteNode("/zk/xaec/tborg/xaec-test_for_zk_common/tns/mavisluo");
+    }
