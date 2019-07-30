24caa586729f5874952d60a91d058228cd5694e7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-30 14:17:15 +0800

    Update zk_comprehensive_test.cpp

--- src/test/zk_comprehensive_test.cpp
+++ src/test/zk_comprehensive_test.cpp
@@ -60,3 +60,4 @@ class MyEnvironment : public testing::Environment
         virtual void TearDown() {
             std::cout << "MyEnvironment TearDown" << std::endl;
             ZookeeperCommon my_zk;
+            int rt = my_zk.Init(FLAGS_cluster_name, FLAGS_zk_servers);
