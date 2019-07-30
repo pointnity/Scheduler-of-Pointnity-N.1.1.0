7292458ccbfb9e70520a11e58ad980523480baf1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-30 14:18:57 +0800

    Update zk_comprehensive_test.cpp

--- src/test/zk_comprehensive_test.cpp
+++ src/test/zk_comprehensive_test.cpp
@@ -72,3 +72,4 @@ class MyEnvironment : public testing::Environment
 
 void PrintMap(std::map<std::string, std::string> one_map) {
     for (std::map<std::string, std::string>::iterator it
+         = one_map.begin(); it != one_map.end(); ++it) {
