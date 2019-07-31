7390bcfd8340d646ce5eb7559498a83dc95f8c67
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-31 17:09:49 +0800

    Update zk_comprehensive_test.cpp

--- src/test/zk_comprehensive_test.cpp
+++ src/test/zk_comprehensive_test.cpp
@@ -96,3 +96,4 @@ TEST(TestZookeeperCommon, TestCreateNode) {
         rt = my_zk.CheckPathExist(test_node);
         EXPECT_EQ(rt, 0);
         /// if you create a node which has no parent, the result is -1
+        rt = my_zk.CreateNode(test_node + "/no_parent/node");
