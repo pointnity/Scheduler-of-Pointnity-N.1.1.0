4ef51ca31ef4bf42b3e6c5bc962d0a46cd1a9712
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-07-31 17:09:29 +0800

    Update zk_comprehensive_test.cpp

--- src/test/zk_comprehensive_test.cpp
+++ src/test/zk_comprehensive_test.cpp
@@ -94,3 +94,4 @@ TEST(TestZookeeperCommon, TestCreateNode) {
         rt = my_zk.CreateNode(test_node);
         EXPECT_EQ(rt, 0);
         rt = my_zk.CheckPathExist(test_node);
+        EXPECT_EQ(rt, 0);
