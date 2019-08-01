ec1332c95823fe76aeba8d50b6f79b51c19535c7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-01 12:10:52 +0800

    Update zookeeper.cpp

--- src/test/zookeeper.cpp
+++ src/test/zookeeper.cpp
@@ -8,3 +8,4 @@ void watcher(zhandle_t *zzh, int32_t type, int32_t state, const char *path, void
 int32_t main(int argc, char* argv[]){
     zoo_set_debug_level(ZOO_LOG_LEVEL_INFO);
     zhandle_t *zh = zookeeper_init("localhost:2181", watcher, 10000, 0, 0, 0);
+    char buffer[512];
