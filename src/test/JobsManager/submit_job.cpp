6a1a6c073cec31a9f374d57d8c2c333da480f657
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-03 16:41:57 +0800

    Update submit_job.cpp

--- src/test/JobsManager/submit_job.cpp
+++ src/test/JobsManager/submit_job.cpp
@@ -40,3 +40,4 @@ int main(int argc, char ** argv) {
     int32_t task_num = atoi(argv[4]);
     if (task_num <= 0) {
         cout << "Usage is wrong." << endl;
+        cout << "Usage is: " << usage << endl;
