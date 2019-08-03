c25f0b18cbaae36049312f960a2eba580885990a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-03 16:45:43 +0800

    Update submit_job.cpp

--- src/test/JobsManager/submit_job.cpp
+++ src/test/JobsManager/submit_job.cpp
@@ -73,3 +73,4 @@ int main(int argc, char ** argv) {
     try {
         Proxy<JobsManagerClient> proxy = RpcClient<JobsManagerClient>::GetProxy(endpoint);
         proxy().SubmitJob(result, str_ad);
+    } catch (TException &tx) {
