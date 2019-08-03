724a7975efa63f5568f54a20085b12d7bb8b3183
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-03 16:42:31 +0800

    Update submit_job.cpp

--- src/test/JobsManager/submit_job.cpp
+++ src/test/JobsManager/submit_job.cpp
@@ -46,3 +46,4 @@ int main(int argc, char ** argv) {
     }
 
     cout << "submit job, job_type:" << job_type << ", task_num:" << task_num << endl;
+    string endpoint = "192.168.10.80:9999"; // hy
