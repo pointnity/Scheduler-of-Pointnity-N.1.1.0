5302e7683278686a7e82f7f09c4d546a690b3355
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-05 11:37:12 +0800

    Update submit_job_daemon.cpp

--- src/test/JobsManager/submit_job_daemon.cpp
+++ src/test/JobsManager/submit_job_daemon.cpp
@@ -149,3 +149,4 @@ int main(int argc, char ** argv) {
     int32_t job_num = 0;
     //create thread to get task result
     //pthread_t task_result_t;
+    //pthread_create(&task_result_t, NULL, TaskResultProcessor, NULL);
