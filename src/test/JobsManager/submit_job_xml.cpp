ab4426f20dc9a6f8315e14d8f3768fc1e7b561b3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-06 17:04:50 +0800

    Update submit_job_xml.cpp

--- src/test/JobsManager/submit_job_xml.cpp
+++ src/test/JobsManager/submit_job_xml.cpp
@@ -12,3 +12,5 @@
     std::ifstream t("file.txt");  
     std::string str((std::istreambuf_iterator<char>(t)),  
                      std::istreambuf_iterator<char>());  
+
+#include "common/clynn/rpc.h"
