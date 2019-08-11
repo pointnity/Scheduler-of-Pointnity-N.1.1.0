4c820c0fec8a779572a810d8718e0a0325869ce1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-11 16:40:26 +0800

    Update start_task.cpp

--- src/test/executor/start_task.cpp
+++ src/test/executor/start_task.cpp
@@ -80,3 +80,4 @@ int main(int argc, char ** argv) {
     ad.InsertAttr(ATTR_VCPU, 1);
     ad.InsertAttr(ATTR_IP, "192.168.10.163");
     ad.InsertAttr(ATTR_PORT, 9991);
+    ad.InsertAttr(ATTR_OS, "ubuntu");
