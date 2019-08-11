cc5120ea03bccf6d8531b6423e8d3681e76203d9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-11 16:41:48 +0800

    Update start_task.cpp

--- src/test/executor/start_task.cpp
+++ src/test/executor/start_task.cpp
@@ -84,3 +84,4 @@ int main(int argc, char ** argv) {
     ad.InsertAttr(ATTR_Image, "ubuntu.qcow2");
     ad.InsertAttr(ATTR_SIZE, 10);
     ad.InsertAttr(ATTR_ISO, "");
+    ad.InsertAttr(ATTR_VNC_PORT, vnc_port);
