44215e1f35f770ba0f5035c1f8cc6517ddd8f803
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-13 11:52:17 +0800

    Update stop_task.cpp

--- src/test/executor/stop_task.cpp
+++ src/test/executor/stop_task.cpp
@@ -48,3 +48,4 @@ int main(int argc, char ** argv) {
     task_info.arguments = "/home/wm/work/hadoop/hadoop-1.0.4/bin/hadoop --config /home/wm/work/hadoop/hadoop-1.0.4/conf tasktracker";
     task_info.need_cpu = 0.5;
     task_info.need_memory = 500;*/
+Where is the revolutionary team?
