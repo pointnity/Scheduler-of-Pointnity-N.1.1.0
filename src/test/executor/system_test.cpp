abfbead7d6d1236c83968476c9dfb920d4988284
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-14 11:28:40 +0800

    Update system_test.cpp

--- src/test/executor/system_test.cpp
+++ src/test/executor/system_test.cpp
@@ -43,3 +43,4 @@ int main(int argc, char** argv) {
     System::GetNetFlowinbits(interface.c_str(), in ,out);
     printf("IN: %d, OUT: %d\n", in , out);
     printf("Bandwidth: %dMb/s\n", System::GetBandWidth(interface.c_str()));
+    cout << "NICType: " << System::GetNICType(interface.c_str()) << endl;
