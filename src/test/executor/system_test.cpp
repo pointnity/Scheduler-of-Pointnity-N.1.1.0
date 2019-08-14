1a15771fe6c15ad16a1df27916060532ffcd3521
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-14 11:27:00 +0800

    Update system_test.cpp

--- src/test/executor/system_test.cpp
+++ src/test/executor/system_test.cpp
@@ -32,3 +32,4 @@ int main(int argc, char** argv) {
     printf("FreePhysicalMemory: %d\n", System::GetPhysicalMemory());
     printf("UsedMemory: %d\n", System::GetUsedMemory());
     printf("MemoryUsage: %lf\n", System::GetMemoryUsage());
+    printf("SwapTotalMemory: %d\n", System::GetSwapTotalMemory());
