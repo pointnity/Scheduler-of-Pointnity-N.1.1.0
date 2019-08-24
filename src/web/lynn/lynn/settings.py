345fd793da4e2b6e80694924796f4c92e4db7400
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-24 11:04:33 +0800

    Update settings.py

--- src/web/lynn/lynn/settings.py
+++ src/web/lynn/lynn/settings.py
@@ -20,3 +20,4 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
+    }
