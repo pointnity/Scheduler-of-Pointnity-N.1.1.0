ad556ec01b3355d883641dec19849f8d2b8765a5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-24 11:04:17 +0800

    Update settings.py

--- src/web/lynn/lynn/settings.py
+++ src/web/lynn/lynn/settings.py
@@ -18,3 +18,4 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
