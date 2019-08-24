76efd01b1692f1743535400d5edefff07673d1c8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-24 11:08:10 +0800

    Update settings.py

--- src/web/lynn/lynn/settings.py
+++ src/web/lynn/lynn/settings.py
@@ -3,10 +3,7 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
+ADMINS 
 MANAGERS = ADMINS
 
 STATIC_PATH='./medias'
@@ -22,9 +19,3 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
-
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
