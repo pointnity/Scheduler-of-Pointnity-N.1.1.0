ddc4da9e7abe1ad745b0d8a488dd7eed5fce9938
Pointity Network <38712949+pointnity@users.noreply.github.com>
2019-08-24 11:04:08 +0800

    Update settings.py

--- src/web/lynn/lynn/settings.py
+++ src/web/lynn/lynn/settings.py
@@ -17,3 +17,4 @@ DATABASES = {
         'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
+        'PASSWORD': '',
