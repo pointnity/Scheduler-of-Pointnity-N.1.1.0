d9c0ee062a549db9c5e16fc88cd310a906873535
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-21 20:49:25 +0800

    Update jquery.bgiframe-2.1.2.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.bgiframe-2.1.2.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.bgiframe-2.1.2.js
@@ -9,3 +9,4 @@
 $.fn.bgiframe = ($.browser.msie && /msie 6\.0/i.test(navigator.userAgent) ? function(s) {
     s = $.extend({
         top     : 'auto', // auto == .currentStyle.borderTopWidth
+        left    : 'auto', // auto == .currentStyle.borderLeftWidth
