bb2b2d8f45ba3c4b9c2f8ff728857400fae8700c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-21 20:54:45 +0800

    Update jquery.bgiframe-2.1.2.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.bgiframe-2.1.2.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.bgiframe-2.1.2.js
@@ -18,3 +18,4 @@ $.fn.bgiframe = ($.browser.msie && /msie 6\.0/i.test(navigator.userAgent) ? func
     var html = '<iframe class="bgiframe"frameborder="0"tabindex="-1"src="'+s.src+'"'+
                    'style="display:block;position:absolute;z-index:-1;'+
                        (s.opacity !== false?'filter:Alpha(Opacity=\'0\');':'')+
+                       'top:'+(s.top=='auto'?'expression(((parseInt(this.parentNode.currentStyle.borderTopWidth)||0)*-1)+\'px\')':prop(s.top))+';'+
