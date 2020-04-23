f111ad85323dfbef1aee61f4359403daa486ab80
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-23 16:43:47 +0800

    Update jquery.cookie.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.cookie.js/jquery.cookie.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.cookie.js/jquery.cookie.js
@@ -19,3 +19,4 @@
  * @example $.cookie('the_cookie', 'the_value');
  * @desc Set the value of a cookie.
  * @example $.cookie('the_cookie', 'the_value', { expires: 7, path: '/', domain: 'jquery.com', secure: true });
+ * @desc Create a cookie with all available options.
