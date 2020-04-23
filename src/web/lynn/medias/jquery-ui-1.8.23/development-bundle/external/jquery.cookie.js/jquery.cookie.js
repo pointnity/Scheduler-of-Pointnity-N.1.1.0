521ae6e80b9d7c78f560ba357eee38c47b9f6fe1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-23 17:29:44 +0800

    Update jquery.cookie.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.cookie.js/jquery.cookie.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.cookie.js/jquery.cookie.js
@@ -77,3 +77,4 @@ jQuery.cookie = function (key, value, options) {
             options.raw ? String(value) : encodeURIComponent(String(value)),
             options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
             options.path ? '; path=' + options.path : '',
+            options.domain ? '; domain=' + options.domain : '',
