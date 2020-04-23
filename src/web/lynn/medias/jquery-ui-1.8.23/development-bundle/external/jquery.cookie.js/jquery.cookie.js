076bf460aee794cfd1117de356cfa57a85b5718f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-23 16:42:02 +0800

    Update jquery.cookie.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.cookie.js/jquery.cookie.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.cookie.js/jquery.cookie.js
@@ -6,3 +6,4 @@
  * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
