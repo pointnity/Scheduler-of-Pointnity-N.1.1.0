39745578e0b9f0fbe9ef8b7bbefc405ac8acfbda
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-27 19:01:40 +0800

    Update jquery.metadata.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.metadata.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.metadata.js
@@ -39,3 +39,4 @@
  * @example <p id="one" class="some_class" data="{item_id: 1, item_label: 'Label'}">This is a p</p>
  * @before $.metadata.setType("attr", "data")
  * @after $("#one").metadata().item_id == 1; $("#one").metadata().item_label == "Label"
+ * @desc Reads metadata from a "data" attribute
