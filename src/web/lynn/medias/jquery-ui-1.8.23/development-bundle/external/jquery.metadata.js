7fbb4018108bc4f121f2a7f74eada9d727cec330
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-27 18:52:38 +0800

    Update jquery.metadata.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.metadata.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/jquery.metadata.js
@@ -4,3 +4,4 @@
  * Copyright (c) 2006 John Resig, Yehuda Katz, J�örn Zaefferer, Paul McLanahan
  *
  * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
