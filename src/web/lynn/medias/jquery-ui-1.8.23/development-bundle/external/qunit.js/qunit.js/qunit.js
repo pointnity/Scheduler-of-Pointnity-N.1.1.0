0f4b70edf41a7cb42b235f1acbd8c73d851d214a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-04-29 16:46:39 +0800

    Update qunit.js

--- src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/qunit.js/qunit.js/qunit.js
+++ src/web/lynn/medias/jquery-ui-1.8.23/development-bundle/external/qunit.js/qunit.js/qunit.js
@@ -53,3 +53,4 @@ Test.prototype = {
 					name: config.previousModule,
 					failed: config.moduleStats.bad,
 					passed: config.moduleStats.all - config.moduleStats.bad,
+					total: config.moduleStats.all
