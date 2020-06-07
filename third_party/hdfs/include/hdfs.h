d39e9b72f3057f8d39fa6d992d202c78f51beb08
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-07 21:10:43 +0800

    Update hdfs.h

--- third_party/hdfs/include/hdfs.h
+++ third_party/hdfs/include/hdfs.h
@@ -69,3 +69,4 @@ extern  "C" {
     /** 
      * hdfsConnectAsUser - Connect to a hdfs file system as a specific user
      * Connect to the hdfs.
+     * @param host A string containing either a host name, or an ip address
