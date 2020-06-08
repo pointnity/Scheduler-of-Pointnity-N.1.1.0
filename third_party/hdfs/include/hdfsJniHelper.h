9d512faa4a66b2ffb19bf6562fbc64244e80d216
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-08 19:07:29 +0800

    Update hdfsJniHelper.h

--- third_party/hdfs/include/hdfsJniHelper.h
+++ third_party/hdfs/include/hdfsJniHelper.h
@@ -53,3 +53,4 @@ int invokeMethod(JNIEnv *env, RetVal *retval, Exc *exc, MethType methType,
  * ctorSignature: the signature of the constructor "(arg-types)V"
  * env: The JNIEnv pointer
  * exc: If the ctor throws any exception, this will contain the reference
+ * Arguments to the ctor must be passed after ctorSignature 
