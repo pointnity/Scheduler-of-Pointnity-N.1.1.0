454acecf7dd396f3b804a71df4a4691123814c68
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-08 19:06:45 +0800

    Update hdfsJniHelper.h

--- third_party/hdfs/include/hdfsJniHelper.h
+++ third_party/hdfs/include/hdfsJniHelper.h
@@ -47,3 +47,7 @@ typedef jthrowable Exc;
 int invokeMethod(JNIEnv *env, RetVal *retval, Exc *exc, MethType methType,
                  jobject instObj, const char *className, const char *methName, 
                  const char *methSignature, ...);
+
+/** constructNewObjectOfClass: Invoke a constructor.
+ * className: Name of the class
+ * ctorSignature: the signature of the constructor "(arg-types)V"
