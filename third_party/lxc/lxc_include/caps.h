d1a0ae85d575f0e83b97428b3d54c4b853e51612
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-10 17:49:58 +0800

    Update caps.h

--- third_party/lxc/lxc_include/caps.h
+++ third_party/lxc/lxc_include/caps.h
@@ -5,3 +5,30 @@ extern int lxc_caps_reset(void);
 extern int lxc_caps_down(void);
 extern int lxc_caps_up(void);
 extern int lxc_caps_init(void);
+
+#define lxc_priv(__lxc_function)			\
+	({						\
+		int __ret, __ret2, __errno = 0;		\
+		__ret = lxc_caps_up();			\
+		if (__ret)				\
+			goto __out;			\
+		__ret = __lxc_function;			\
+		if (__ret)				\
+			__errno = errno;		\
+		__ret2 = lxc_caps_down();		\
+	__out:	__ret ? errno = __errno,__ret : __ret2;	\
+	})
+
+#define lxc_unpriv(__lxc_function)		\
+	({						\
+		int __ret, __ret2, __errno = 0;		\
+		__ret = lxc_caps_down();		\
+		if (__ret)				\
+			goto __out;			\
+		__ret = __lxc_function;			\
+		if (__ret)				\
+			__errno = errno;		\
+		__ret2 = lxc_caps_up();			\
+	__out:	__ret ? errno = __errno,__ret : __ret2;	\
+	})
+#endif
