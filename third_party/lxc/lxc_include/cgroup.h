ba8dd09114a4a71b092fdb6879e0ca360a9f67be
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-11 18:15:01 +0800

    Update cgroup.h

--- third_party/lxc/lxc_include/cgroup.h
+++ third_party/lxc/lxc_include/cgroup.h
@@ -6,3 +6,5 @@
 struct lxc_handler;
 int lxc_cgroup_create(const char *name, pid_t pid);
 int lxc_cgroup_destroy(const char *name);
+int lxc_cgroup_path_get(char **path, const char *subsystem, const char *name);
+int lxc_cgroup_nrtasks(const char *name);
