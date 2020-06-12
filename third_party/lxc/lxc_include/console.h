44915f67e483f5ee1c084bff2251d81f1739cf58
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-12 10:24:15 +0800

    Update console.h

--- third_party/lxc/lxc_include/console.h
+++ third_party/lxc/lxc_include/console.h
@@ -1,2 +1,3 @@
 extern int lxc_create_console(struct lxc_conf *);
 extern void lxc_delete_console(struct lxc_console *);
+extern int lxc_console_mainloop_add(struct lxc_epoll_descr *, struct lxc_handler *);
