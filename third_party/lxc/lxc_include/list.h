302afae028bc504fc264cc6475b13fec9b5732bf
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-12 11:20:14 +0800

    Update list.h

--- third_party/lxc/lxc_include/list.h
+++ third_party/lxc/lxc_include/list.h
@@ -54,3 +54,7 @@ static inline void lxc_list_add(struct lxc_list *head, struct lxc_list *list)
 {
 	__lxc_list_add(list, head, head->next);
 }
+
+static inline void lxc_list_add_tail(struct lxc_list *head,
+				     struct lxc_list *list)
+{
