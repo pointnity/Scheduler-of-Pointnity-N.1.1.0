52dcaebea37460076a48bae972d1984e625b6f54
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-15 17:44:27 +0800

    Update monitor.h

--- third_party/lxc/lxc_include/monitor.h
+++ third_party/lxc/lxc_include/monitor.h
@@ -16,3 +16,4 @@ struct lxc_msg {
 
 void lxc_monitor_send_state(const char *name, lxc_state_t state);
 void lxc_monitor_send_priority(const char *name, int priority);
+void lxc_monitor_cleanup(const char *name);
