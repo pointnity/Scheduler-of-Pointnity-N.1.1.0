401d42ab74cbcc0b217447fbd46a553a95e4ad42
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-17 17:43:02 +0800

    Update state.h

--- third_party/lxc/lxc_include/state.h
+++ third_party/lxc/lxc_include/state.h
@@ -10,3 +10,4 @@ extern int lxc_rmstate(const char *name);
 extern lxc_state_t lxc_getstate(const char *name);
 
 extern lxc_state_t lxc_str2state(const char *state);
+extern const char *lxc_state2str(lxc_state_t state);
