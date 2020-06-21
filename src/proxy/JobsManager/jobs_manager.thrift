bf5a71eac58920a166675a60fd3361b9e2f53273
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-21 19:07:46 +0800

    Update jobs_manager.thrift

--- src/proxy/JobsManager/jobs_manager.thrift
+++ src/proxy/JobsManager/jobs_manager.thrift
@@ -36,3 +36,4 @@ service JobsManager {
    bool TaskStarted(1:i32 job_id, 2:i32 task_id),
    bool TaskFinished(1:i32 job_id, 2:i32 task_id),
    bool TaskFailed(1:i32 job_id, 2:i32 task_id),
+   bool TaskMissed(1:i32 job_id, 2:i32 task_id),
