39dd37c0413c2bd0da4d85363517fea13267803d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-06-30 09:25:10 +0800

    Update checker.cpp

--- src/executor/checker.cpp
+++ src/executor/checker.cpp
@@ -10,3 +10,5 @@
 #include <log4cplus/loggingmacros.h>
 #include <log4cplus/loggingmacros.h>
 #include <log4cplus/loggingmacros.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/loggingmacros.h>
