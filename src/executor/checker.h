75ce87b1bbdf5d17ae876003c8a4bb197244736c
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-01 19:38:45 +0800

    Update checker.h

--- src/executor/checker.h
+++ src/executor/checker.h
@@ -1,17 +1,1 @@
 /********************************
- FileName: executor/image_maneger.h
- Version:  0.1
- Description: image manager
-*********************************/
-
-#ifndef SRC_EXECUTOR_CHECKER_H
-#define SRC_EXECUTOR_CHECKER_H
-
-#include <string>
-
-class Checker {
-public:
-    bool CheckVersion();
-};
-
-#endif  
