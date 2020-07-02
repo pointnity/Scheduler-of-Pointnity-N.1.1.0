904c61d549370b35f7d5ff3b9942e85275448775
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-02 16:25:16 +0800

    Update clean_environment.cpp

--- src/executor/clean_environment.cpp
+++ src/executor/clean_environment.cpp
@@ -1,9 +1,5 @@
 /********************************
  FileName: executor/clean_environment.h
- FileName: executor/clean_environment.h
  Version:  0.1
- Version:  0.1
- Version:  0.1
- Description: clean environment, include libvirt and lxc
  Description: clean environment, include libvirt and lxc
 *********************************/
