a0ea5547c0a234cca791140ddc8bf8e831ea4490
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-03 15:23:51 +0800

    Update clean_environment.cpp

--- src/executor/clean_environment.cpp
+++ src/executor/clean_environment.cpp
@@ -40,3 +40,4 @@ int32_t CleanLibvirt() {
         virDomainPtr dom = virDomainLookupByID(conn, active_domains[i]);
         if (NULL == dom) {
             printf("Failed to find domain %d\n", active_domains[i]);
+            continue;
