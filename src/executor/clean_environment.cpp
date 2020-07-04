f9cef6619901b1a5fedae0c3d3f1f57851506f2f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-04 11:36:01 +0800

    Update clean_environment.cpp

--- src/executor/clean_environment.cpp
+++ src/executor/clean_environment.cpp
@@ -71,4 +71,5 @@ int32_t CleanIPRules() {
     // clean iptables rules
     string cmd = "iptables -D FORWARD -i eth0 -o virbr0 -p tcp -m state --state NEW -j ACCEPT";
     system(cmd.c_str());
-    system(cmd.c_str());
+  
+    // clean files
