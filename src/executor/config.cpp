c5f850f2771bd246e2df6fcdccc51d3e03b12d14
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-05 15:53:04 +0800

    Update config.cpp

--- src/executor/config.cpp
+++ src/executor/config.cpp
@@ -33,5 +33,4 @@ DEFINE_string(ip_range, "192.168.10.80-192.168.10.90", "range of ip addrs");
 // debug
 DEFINE_bool(debug, false, "print some debug information");
 DEFINE_bool(jobs_manager_up, true, "JobsManager up");
-DEFINE_bool(jobs_manager_up, true, "JobsManager up");
  
