a053e1910ef0c92857f08d4587271f83f3afbf54
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-05 14:01:24 +0800

    Update config.cpp

--- src/executor/config.cpp
+++ src/executor/config.cpp
@@ -11,3 +11,4 @@ DEFINE_string(resource_scheduler_endpoint, "127.0.0.1:9998", "ResouceScheduler e
 DEFINE_string(jobs_manager_endpoint, "127.0.0.1:9999", "JobsManager endpoint");
 DEFINE_int32(hb_interval, 15, "heartbeat interval");
 DEFINE_int32(vm_hb_interval, 15, "vm heartbeat interval");
+DEFINE_string(interface, "br0", "network interface, communicate with master");
