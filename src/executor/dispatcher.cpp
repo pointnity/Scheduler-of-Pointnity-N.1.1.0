b5f066ac19443c7bb9984ae8060e8b66575a6010
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-07 15:21:24 +0800

    Update dispatcher.cpp

--- src/executor/dispatcher.cpp
+++ src/executor/dispatcher.cpp
@@ -1,16 +1,1 @@
 /********************************
- FileName: executor/dispatcher.cpp
- Version:  0.1
- Description: dispatcher for events; events -> dispatcher -> handler
-*********************************/
-
-#include "executor/dispatcher.h"
-
-// register <EventType::type, Handler*>
-void Dispatcher::Register(EventType::type type, Handler* handler) {
-    m_handler_map[type] = handler;
-}
-
-Handler* Dispatcher::Dispatch(EventType::type type) {
-    return m_handler_map[type];
-}
