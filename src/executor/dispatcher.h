80706340ad7f3d5f709b7da587ac4c7ee60471f4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-08 15:58:08 +0800

    Update dispatcher.h

--- src/executor/dispatcher.h
+++ src/executor/dispatcher.h
@@ -3,3 +3,6 @@
  Version:  0.1
  Description: dispatcher for events; events -> dispatcher -> handler
 *********************************/
+
+#ifndef SRC_EXECUTOR_DISPATCHER_H
+#define SRC_EXECUTOR_DISPATCHER_H
