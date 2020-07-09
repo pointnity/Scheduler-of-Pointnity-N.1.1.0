7f221eb5b8b172f270af4204339e5d3993de896f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-09 13:40:52 +0800

    Update event.cpp

--- src/executor/event.cpp
+++ src/executor/event.cpp
@@ -37,6 +37,7 @@ bool StartActionEvent::Handle() {
 }
 
 // kill task
+bool KillActionEvent::Handle() {
     TaskID id = GetID();
     if(FLAGS_debug) {
     LOG4CPLUS_DEBUG(logger, "Trigger event of kill task action, job_id:" << id.job_id << ", task_id:" << id.task_id);
