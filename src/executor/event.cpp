ceaf3a6eeb0dfb1112e94af9adb59a533240dd6d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-09 13:35:51 +0800

    Update event.cpp

--- src/executor/event.cpp
+++ src/executor/event.cpp
@@ -35,3 +35,5 @@ bool StartActionEvent::Handle() {
     LOG4CPLUS_INFO(logger, "Handle event of start task action, job_id:" << id.job_id << ", task_id:" << id.task_id);
     return true;
 }
+
+// kill task
