a986b8f3e27e23245b1d20fc0b91957fc18a5642
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-10 21:24:36 +0800

    Update event.cpp

--- src/executor/event.cpp
+++ src/executor/event.cpp
@@ -43,3 +43,4 @@ bool KillActionEvent::Handle() {
     LOG4CPLUS_DEBUG(logger, "Trigger event of kill task action, job_id:" << id.job_id << ", task_id:" << id.task_id);
     }
     if (!TaskPoolI::Instance()->KillTaskByID(id)) {
+        LOG4CPLUS_ERROR(logger, "Failed to kill task, job_id:" << id.job_id << ", task_id:" << id.task_id);
