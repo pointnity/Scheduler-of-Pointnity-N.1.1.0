97324fbfc927af4aaeab6df25377def2280a27e9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-11 19:43:08 +0800

    Update event.cpp

--- src/executor/event.cpp
+++ src/executor/event.cpp
@@ -67,3 +67,4 @@ bool StartingTaskEvent::Handle() {
     }
     TaskPoolI::Instance()->GetTaskPtr(id)->TaskStarting();
     LOG4CPLUS_INFO(logger, "Handle event of taskstarting state, job_id:" << id.job_id << ", task_id:" << id.task_id);
+    return true;
