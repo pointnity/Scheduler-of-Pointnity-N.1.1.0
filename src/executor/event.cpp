128af43114e68bc6dfc8b973eb2fc6d14189a26d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-11 19:40:50 +0800

    Update event.cpp

--- src/executor/event.cpp
+++ src/executor/event.cpp
@@ -63,3 +63,4 @@ bool StopActionEvent::Handle() {
 bool StartingTaskEvent::Handle() {
     TaskID id = GetID();
     if(FLAGS_debug) {
+        LOG4CPLUS_DEBUG(logger, "Trigger event of taskstarting state, job_id:" << id.job_id << ", task_id:" << id.task_id);
