210d6f8cf5cb31520061b9d757d38aa8a23d0011
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-16 11:24:02 +0800

    Update executor_main.cpp

--- src/executor/executor_main.cpp
+++ src/executor/executor_main.cpp
@@ -58,3 +58,4 @@ extern void* HeartbeatProcessor(void* unused);
 // executor
 int ExecutorEntity(int argc, char **argv) {
     // config file
+        google::ParseCommandLineFlags(&argc, &argv, true);
