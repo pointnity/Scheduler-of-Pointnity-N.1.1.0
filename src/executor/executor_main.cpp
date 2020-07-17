92e6630bf6325980d3daa53b958deed6218e8458
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-17 14:46:05 +0800

    Update executor_main.cpp

--- src/executor/executor_main.cpp
+++ src/executor/executor_main.cpp
@@ -72,3 +72,4 @@ int ExecutorEntity(int argc, char **argv) {
 
     // initilize log log4cplus
     SharedObjectPtr<Appender> append(new FileAppender(FLAGS_log_path + "/executor.log"));
+    append->setName(LOG4CPLUS_TEXT("append for executor"));
