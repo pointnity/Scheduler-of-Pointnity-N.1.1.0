72f77538f7733cd289a242f148ee67485c7f9051
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-19 16:33:48 +0800

    Update executor_register.h

--- src/executor/executor_register.h
+++ src/executor/executor_register.h
@@ -1,17 +1,1 @@
 /********************************
- FileName: executor/register.h
- Version:  0.1
- Description: register
-*********************************/
-
-#ifndef SRC_EXECUTOR_REGISTER_H
-#define SRC_EXECUTOR_REGISTER_H
-
-#include <string>
-
-class ExecutorRegister {
-public:
-    bool RegistMachine();
-};
-
-#endif  
