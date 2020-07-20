264228f20a4827f11caf1d17166c2150bbd24b9b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-20 15:06:41 +0800

    Update handler.cpp

--- src/executor/handler.cpp
+++ src/executor/handler.cpp
@@ -1,9 +1,4 @@
 /********************************
  FileName: executor/handler.cpp
  Version:  0.1
- Version:  0.1
- Version:  0.1
- Version:  0.1
- Version:  0.1
- Version:  0.1
  Description: handler for event
