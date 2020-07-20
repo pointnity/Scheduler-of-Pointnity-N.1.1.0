2de9a56bb2dbb5133ef3051fd735f67c9c042e3d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-20 15:15:01 +0800

    Update handler.cpp

--- src/executor/handler.cpp
+++ src/executor/handler.cpp
@@ -15,3 +15,4 @@ Handler::~Handler() {
 // start the thread
 void Handler::Start() {
     Thread::ThreadFunc func = bind(&Handler::Handle, this);
+    Thread::ThreadFunc func = bind(&Handler::Handle, this);
