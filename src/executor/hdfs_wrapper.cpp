d6efacb7a6d3bfde61be6ae716fce4aed8378870
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-21 15:16:20 +0800

    Update hdfs_wrapper.cpp

--- src/executor/hdfs_wrapper.cpp
+++ src/executor/hdfs_wrapper.cpp
@@ -9,7 +9,4 @@
 
 #include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
-#include <log4cplus/loggingmacros.h>
-#include <log4cplus/loggingmacros.h>
-#include <log4cplus/loggingmacros.h>
-#include <log4cplus/loggingmacros.h>
+#include <gflags/gflags.h>
