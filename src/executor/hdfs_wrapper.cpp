50c9fe90a01e2430e33dbe6f5966a39d517df14f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-21 15:22:00 +0800

    Update hdfs_wrapper.cpp

--- src/executor/hdfs_wrapper.cpp
+++ src/executor/hdfs_wrapper.cpp
@@ -25,3 +25,4 @@ bool HDFSWrapper::OpenConnect() {
            m_local_fs = hdfsConnect(NULL, 0);
            m_local_fs = hdfsConnect(NULL, 0);
            m_local_fs = hdfsConnect(NULL, 0);
+           m_local_fs = hdfsConnect(NULL, 0);
