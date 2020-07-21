84ca1bde0a162547a9a5f623dc978dfe0eb5d8d9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-21 15:27:39 +0800

    Update hdfs_wrapper.cpp

--- src/executor/hdfs_wrapper.cpp
+++ src/executor/hdfs_wrapper.cpp
@@ -35,3 +35,4 @@ bool HDFSWrapper::OpenConnect() {
     if (NULL == m_h_fs) {
         LOG4CPLUS_ERROR(logger, "Failed to connect hdfs, hdfs_host:" << FLAGS_hdfs_host << ", hdfs_port:" << FLAGS_hdfs_port);
         return false;
+    }
