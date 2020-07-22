7e7a174ce22b21d360fbb54593939eb0962d7bd1
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-22 15:39:05 +0800

    Update hdfs_wrapper.cpp

--- src/executor/hdfs_wrapper.cpp
+++ src/executor/hdfs_wrapper.cpp
@@ -49,3 +49,4 @@ bool HDFSWrapper::OpenConnect() {
     string local_path2 = "/home/wm/b.cpp";
     string h_path2 = "/user/root/b.cpp";
     if (!CopyFromLocalFile(local_path2, h_path2)) {
+        return false;
