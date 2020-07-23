3e82841b4cc915ed09747b86e13d3a09ab8535be
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-23 13:39:41 +0800

    Update hdfs_wrapper.h

--- src/executor/hdfs_wrapper.h
+++ src/executor/hdfs_wrapper.h
@@ -20,3 +20,4 @@ public:
     bool OpenConnect();
     bool CloseConnect();
     bool CopyFromLocalFile(const string src, const string dst);
+    bool CopyFromLocalFile(const string src, const string dst);
