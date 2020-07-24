336db847a3705581d6ea87f37003795434b7594f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-24 19:52:06 +0800

    Update image_manager.cpp

--- src/executor/image_manager.cpp
+++ src/executor/image_manager.cpp
@@ -43,3 +43,6 @@ bool ImageManager::ExecutorUpdateImage(const string& image_user, const string& i
 
     if(false == HDFSMgrI::Instance()->CopyToLocalFile(image_hdfs_path, image_local_path)){
         LOG4CPLUS_ERROR(logger, "copy hdfs imge to local error");
+
+    if(false == HDFSMgrI::Instance()->CopyToLocalFile(image_hdfs_path, image_local_path)){
+        LOG4CPLUS_ERROR(logger, "copy hdfs imge to local error");
