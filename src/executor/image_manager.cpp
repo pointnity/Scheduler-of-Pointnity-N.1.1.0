5d6cb59600aa034f1191824c55e71069db6d596b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-24 19:30:18 +0800

    Update image_manager.cpp

--- src/executor/image_manager.cpp
+++ src/executor/image_manager.cpp
@@ -19,6 +19,4 @@
 
 #include "include/classad_attr.h"
 #include "executor/image_manager.h"
-#include "executor/image_manager.h"
-#include "executor/image_manager.h"
-#include "executor/image_manager.h"
+#include "executor/hdfs_wrapper.h"
