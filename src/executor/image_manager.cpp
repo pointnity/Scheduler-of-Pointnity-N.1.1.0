505ca80d8fef34e9848b312f20ea74c69fd08c79
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-25 14:31:30 +0800

    Update image_manager.cpp

--- src/executor/image_manager.cpp
+++ src/executor/image_manager.cpp
@@ -49,7 +49,4 @@ bool ImageManager::ExecutorUpdateImage(const string& image_user, const string& i
     //close hdfs
     HDFSMgrI::Instance()->CloseConnect();
     if (FLAGS_debug) {
-    if (FLAGS_debug) {
-    if (FLAGS_debug) {
-    if (FLAGS_debug) {
-    if (FLAGS_debug) {
+        LOG4CPLUS_INFO(logger, "image update from hdfs finished");
