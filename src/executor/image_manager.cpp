3c2d5cbdd1fafe8c5b2207fa2ee88c8d9679c8a2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-07-26 16:39:45 +0800

    Update image_manager.cpp

--- src/executor/image_manager.cpp
+++ src/executor/image_manager.cpp
@@ -147,3 +147,4 @@ string ImageManager::GetImageAttrFromFile() {
     //open file error ?
     if(!out_file) {
         LOG4CPLUS_ERROR(logger, "Failed to open image conf file.");
+        return "";
