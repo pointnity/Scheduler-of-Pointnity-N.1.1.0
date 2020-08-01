2b8978d6c12468c44cfcd334b42539fa048d59f7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-01 16:05:17 +0800

    Update kvm.cpp

--- src/executor/kvm.cpp
+++ src/executor/kvm.cpp
@@ -11,7 +11,5 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/time.h>
-#include <sys/time.h>
-#include <sys/time.h>
-#include <sys/time.h>
-#include <sys/time.h>
+
+#include <log4cplus/logger.h>
