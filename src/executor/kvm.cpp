8959bad9c1745b2d2e4d8f2ad8362d4f15878f48
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-03 15:40:47 +0800

    Update kvm.cpp

--- src/executor/kvm.cpp
+++ src/executor/kvm.cpp
@@ -107,3 +107,4 @@ bool KVM::Kill() {
     if (!m_domain_ptr) {
         LOG4CPLUS_ERROR(logger, "Invalid domain pointer, Kill VM directly.");
         // delete work dir
+        // delete work dir
