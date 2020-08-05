5c93e30c86b716bd89d96433280ec5a68c3bdffc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-05 17:10:37 +0800

    Update kvm.cpp

--- src/executor/kvm.cpp
+++ src/executor/kvm.cpp
@@ -233,3 +233,5 @@ void KVM::SetHbVMInfo(const string& hb_vm_info_ad) {
         LOG4CPLUS_ERROR(logger, "Parse " << ATTR_TASK_ID << " error.");
         return;
     }
+
+    if (!ad_ptr->EvaluateAttrNumber(ATTR_VMHB_CPU, m_hb_vm_info.cpu_usage)) {
