09bce7859c62d887539d38421e66df0ffc81022a
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-10 12:23:47 +0800

    Update lxc.cpp

--- src/executor/lxc.cpp
+++ src/executor/lxc.cpp
@@ -2,4 +2,4 @@
  FileName: executor/lxc.cpp
  Version:  0.1
  Description: lxc(Linux Container), inherit from vm
- Description: lxc(Linux Container), inherit from vm
+*********************************/
