4022300e2f053d2f21f61586de880e661129e027
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-11 14:57:40 +0800

    Update lxc.cpp

--- src/executor/lxc.cpp
+++ src/executor/lxc.cpp
@@ -103,3 +103,4 @@ int32_t LXC::StartApp() {
     if (ExecuteLXC() != 0) {
         LOG4CPLUS_ERROR(logger, "Failed to execute lxc, name:" << GetName() << ", job_id:" << GetID().job_id << ", task_id:" << GetID().task_id);
         return -1;
+    }
