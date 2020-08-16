adc8d105974a9db4c35b491989e8b784d94e6ee5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-16 16:08:49 +0800

    Update resource_manager.cpp

--- src/executor/resource_manager.cpp
+++ src/executor/resource_manager.cpp
@@ -143,3 +143,4 @@ void ResourceManager::GenerateHb(string& machine_str_ad, vector<string>& task_li
     // classad
     ClassAd ad;
     ad.InsertAttr(ATTR_Machine, m_name);
+    ad.InsertAttr(ATTR_MachineType, m_machine_type);
