c527e4f7c46b0857f5fcd8cba1e3e84a4d017b20
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-16 16:11:27 +0800

    Update resource_manager.cpp

--- src/executor/resource_manager.cpp
+++ src/executor/resource_manager.cpp
@@ -154,3 +154,4 @@ void ResourceManager::GenerateHb(string& machine_str_ad, vector<string>& task_li
     ad.InsertAttr(ATTR_TotalMemory, m_total_memory);
     ad.InsertAttr(ATTR_TotalDisk, m_total_disk);
     ad.InsertAttr(ATTR_AvailCPUNum, m_avail_cpu);
+    ad.InsertAttr(ATTR_AvailMemory, m_avail_memory);
