93793af16491a6771b948b600fe81a1eef568437
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-16 15:26:31 +0800

    Update resource_manager.cpp

--- src/executor/resource_manager.cpp
+++ src/executor/resource_manager.cpp
@@ -112,3 +112,4 @@ string ResourceManager::GetBridgeEndpoint() {
 void ResourceManager::GenerateHb(string& machine_str_ad, vector<string>& task_list) {
     HbMachineInfo hb_m_info;
     // node machine
+    hb_m_info.ip = m_ip;
