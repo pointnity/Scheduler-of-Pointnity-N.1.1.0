f6aaf5753d7894f68bca1fec30094a90b26bb947
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-18 08:24:27 +0800

    Update resource_manager.cpp

--- src/executor/resource_manager.cpp
+++ src/executor/resource_manager.cpp
@@ -208,3 +208,4 @@ void ResourceManager::AllocateResource(double cpu, int32_t memory, int32_t disk)
 void ResourceManager::ReleaseResource(double cpu, int32_t memory, int32_t disk) {
     WriteLocker locker(m_lock);
     m_avail_cpu += cpu;
+    m_avail_memory += memory;
