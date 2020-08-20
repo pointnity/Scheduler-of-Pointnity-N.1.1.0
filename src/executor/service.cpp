e1930580182ed008b54a696eec387b2adef6ed05
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-20 17:15:42 +0800

    Update service.cpp

--- src/executor/service.cpp
+++ src/executor/service.cpp
@@ -44,3 +44,4 @@ bool ExecutorService::StartTask(const string& info) {
     }
     if (false == TaskPoolI::Instance()->InsertIfAbsent(ptr)) {
         LOG4CPLUS_ERROR(logger, "The task had exist in the executor, job_id:" << ptr->GetID().job_id << ", task_id:" << ptr->GetID().task_id);
+        TaskPoolI::Instance()->PrintAll();
