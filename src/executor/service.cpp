c968faef25166f62921f3ee24e814a7aa51fee31
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-21 14:32:48 +0800

    Update service.cpp

--- src/executor/service.cpp
+++ src/executor/service.cpp
@@ -75,3 +75,6 @@ bool ExecutorService::KillTask(const int32_t job_id, const int32_t task_id) {
     EventDispatcherI::Instance()->Dispatch(event->GetType())->PushBack(event);
     return true;
 }
+
+bool ExecutorService::KillTaskForFT(const int32_t job_id, const int32_t task_id) {
+    if (job_id < 0 || task_id < 0) {
