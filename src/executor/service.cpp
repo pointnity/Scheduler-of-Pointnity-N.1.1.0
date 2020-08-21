5a17f4f1f220475e1f2cb31bbcf8a9f9a46db113
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-21 14:37:03 +0800

    Update service.cpp

--- src/executor/service.cpp
+++ src/executor/service.cpp
@@ -79,3 +79,4 @@ bool ExecutorService::KillTask(const int32_t job_id, const int32_t task_id) {
 bool ExecutorService::KillTaskForFT(const int32_t job_id, const int32_t task_id) {
     if (job_id < 0 || task_id < 0) {
         LOG4CPLUS_ERROR(logger, "TaskID is illegal, job_id:" << job_id << ", task_id:" << task_id);
+        return false;
