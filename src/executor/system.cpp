c9091b213a516eb89e267ccf877df4015b919512
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-27 15:53:25 +0800

    Update system.cpp

--- src/executor/system.cpp
+++ src/executor/system.cpp
@@ -294,3 +294,316 @@ int32_t System::GetSwapFreeMemory() {
     // fs /proc
     FILE* fp = fopen("/proc/meminfo", "r");
     if (!fp)
+        return -1;
+
+    int32_t swap_free = -1;
+    char buf[256] = {0};
+    // get SwapFree from /proc/meminfo
+    while (fgets(buf, sizeof(buf) - 1, fp)) {
+        if (0 == strncmp(buf, "SwapFree", 8)) {
+            if (sscanf(buf, "SwapFree: %d", &swap_free) < 1) {
+                fclose(fp);
+                return -1;
+            }
+            break;
+        }
+    }
+   
+    // close file
+    fclose(fp);
+    swap_free /= 1024;
+    return swap_free;
+}
+
+/// @brief: network flow
+
+// get ip by interface
+int32_t System::GetIP(const char* interface, char* ip) {
+    int32_t sock;
+    struct sockaddr_in sin;
+    struct ifreq ifr;
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    strncpy(ifr.ifr_name, interface, IFNAMSIZ);
+    ifr.ifr_name[IFNAMSIZ - 1] = 0;
+    if (ioctl(sock, SIOCGIFADDR, &ifr) < 0) {
+        perror("ioctl");
+        return -1;
+    }
+
+    memcpy(&sin, &ifr.ifr_addr, sizeof(sin));
+    char * tmp = inet_ntoa(sin.sin_addr);
+    strncpy(ip, tmp, strlen(tmp));
+    return 0;
+}
+
+string System::GetIP(const char* interface) {
+    char ip[16] = {0};
+    GetIP(interface, ip);
+    return ip;
+}
+
+// get bytes_in and bytes_out of a interface
+void System::GetNetFlowinBytes(const char* interface, int32_t& bytes_in, int32_t& bytes_out) {
+    // cleared 0
+    bytes_in = bytes_out = 0;
+
+    // fs /proc
+    FILE *fp = fopen("/proc/net/dev", "r");
+    if(!fp)
+        return;
+
+    char buf[256];
+    char if_name[30];
+    long rb, rpkt, r_err, r_drop, r_fifo, r_frame, r_compr, r_mcast;
+    long tb, tpkg, t_err, t_drop, t_fifo, t_frame, t_compr, t_mcast;
+
+    // read interface into if_name
+    snprintf(if_name, 30, "%s:", interface);
+
+    // read bytes from /proc/net/dev
+    while (fgets(buf, sizeof(buf) - 1, fp)) {
+        char* p = buf;
+        // remove space(qu diao kong ge)
+        while (' ' == *p)
+            p++;
+
+        if (!strncmp(p, if_name, strlen(if_name))) {
+            // 
+            char* b = strstr(p, if_name);
+            sscanf(b + strlen(if_name), "%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld", &rb, &rpkt, &r_err, &r_drop, &r_fifo, &r_frame, &r_compr, &r_mcast, &tb, &tpkg, &t_err, &t_drop, &t_fifo, &t_frame, &t_compr, &t_mcast);
+            bytes_in = rb;
+            bytes_out = tb;
+        }
+    }
+    // close file
+    fclose(fp);
+}
+
+void System::GetNetFlowinbits(const char* interface, int32_t& bits_in, int32_t& bits_out) {
+    // cleared 0
+    bits_in = bits_out = 0;
+
+    // fs /proc
+    FILE *fp = fopen("/proc/net/dev", "r");
+    if (!fp)
+        return;
+
+    char buf[256];
+    char if_name[30];
+    long rb, rpkt, r_err, r_drop, r_fifo, r_frame, r_compr, r_mcast;
+    long tb, tpkg, t_err, t_drop, t_fifo, t_frame, t_compr, t_mcast;
+
+    // read interface into if_name
+    snprintf(if_name, 30, "%s:", interface);
+
+    // read bytes from /proc/net/dev
+    while (fgets(buf, sizeof(buf) - 1, fp)) {
+        char* p = buf;
+        // remove space(qu diao kong ge)
+        while (' ' == *p)
+            p++;
+
+        if (!strncmp(p, if_name, strlen(if_name))) {
+            // 
+            char* b = strstr(p, if_name);
+            sscanf(b + strlen(if_name), "%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld", &rb, &rpkt, &r_err, &r_drop, &r_fifo, &r_frame, &r_compr, &r_mcast, &tb, &tpkg, &t_err, &t_drop, &t_fifo, &t_frame, &t_compr, &t_mcast);
+            bits_in = rb * 8;
+            bits_out = tb * 8;
+        }
+    }
+    // close file
+    fclose(fp);
+}
+
+// get static net bandwidth
+int32_t System::GetBandWidth(const char* interface) {
+    char cmd[30] = {0};
+    // cmd "ethtool eth0"
+    snprintf(cmd, 30, "ethtool %s:", interface);
+
+    FILE *stream = popen(cmd, "r");
+    if (NULL == stream)
+        return -1;
+
+    int32_t bandwidth = -1;
+    char buf[256] = {0};
+    string s_buf = "";
+    // get Speed from ethtool
+    while (fgets(buf, sizeof(buf) - 1, stream)) {
+        s_buf = buf;
+        trim(s_buf);
+         
+        if (0 == strncmp(s_buf.c_str(), "Speed:", 6)) {
+            if (sscanf(s_buf.c_str(), "Speed: %dMb/s", &bandwidth) < 1) {
+                fclose(stream);
+                return -1;
+            }
+            break;
+        }
+    }
+    
+    // close stream
+    pclose(stream);
+    return bandwidth;
+}
+
+// get interface type
+string System::GetNICType(const char* interface) {
+    char cmd[40] = {0};
+    // cmd: "ethtool -i eth0"
+    snprintf(cmd, 40, "ethtool -i %s:", interface);
+
+    FILE *stream = popen(cmd, "r");
+    if (NULL == stream)
+        return NULL;
+
+    string nic_type = "a";
+    char buf[256] = {0};
+    char type[20] = {0};
+
+    // get Speed from ethtool
+    while (fgets(buf, sizeof(buf) - 1, stream)) {
+        if (0 == strncmp(buf, "driver:", 7)) {
+            if (sscanf(buf, "driver: %s", type) < 1) {
+                fclose(stream);
+                return NULL;
+            }
+            break;
+        }
+    }
+    
+
+    // close stream
+    nic_type = type;
+    pclose(stream);
+    return nic_type; 
+}
+
+// @return total disk, size is G
+int32_t System::GetTotalDisk() {
+    // cmd 
+    string cmd = "df --block-size=G";
+    FILE *stream = popen(cmd.c_str(),"r");
+    if (NULL == stream)
+        return -1;
+
+    char buf[1024];
+    float percentage;
+    char location[100], mount_on[100];
+    int32_t capacity, used, available;
+    // read disk info
+    while (fgets(buf, sizeof(buf), stream)) {
+        char* b = strstr(buf," /\n");
+        if(b == NULL) {
+            continue;
+        }
+        sscanf(buf, "%s %dG %dG %dG %f%% %s", location, &capacity, &used, &available, &percentage, mount_on);
+    }
+
+    // close stream
+    pclose(stream);
+    return capacity;
+}
+
+// @return used disk, size is G
+int32_t System::GetUsedDisk() {
+    // cmd 
+    string cmd = "df --block-size=G";
+    FILE *stream = popen(cmd.c_str(),"r");
+    if (NULL == stream)
+        return -1;
+
+    char buf[1024];
+    float percentage;
+    char location[100], mount_on[100];
+    int32_t capacity, used, available;
+    // read disk info
+    while (fgets(buf, sizeof(buf), stream)) {
+        char* b = strstr(buf," /\n");
+        if (b == NULL) {
+            continue;
+        }
+        sscanf(buf, "%s %dG %dG %dG %f%% %s", location, &capacity, &used, &available, &percentage, mount_on);
+    }
+
+    // close stream
+    pclose(stream);
+    return used;
+}
+
+// @return: Disk usage
+double System::GetDiskUsage() {
+    int32_t total = System::GetTotalDisk();
+    int32_t used = System::GetUsedDisk();
+
+    if ((-1 == total) || (-1 == used) || (total < used))
+        return -1;
+    return (double)used/total;
+}
+
+
+/// @brief: others
+// @return: Linux os version
+string System::GetOSVersion() {
+    // read cmd "uname -r"
+    string cmd = "uname -r";
+    FILE *stream = popen(cmd.c_str(),"r");
+    if (NULL == stream)
+        return NULL;
+
+    // get version
+    char buf[256];
+    string version;
+    fgets(buf, sizeof(buf), stream);
+
+    // remove '\n'
+    int32_t len = strlen(buf);
+    buf[len-1] = '\0';
+    version = buf;
+    
+    // close stream
+    pclose(stream);
+    return "Linux " + version;
+}
+
+// remove a dir, not a file
+void System::RemoveDir(const char* path) {
+    DIR* dp = opendir(path);
+    if (dp) { 
+        dirent* ep = NULL;
+        while ((ep = readdir(dp)) != NULL) {
+            // get rid of '..' and '.'
+            if (strcmp("..", ep->d_name) != 0 &&
+                strcmp(".", ep->d_name) != 0) {
+                char file[256] = {0};
+                snprintf(file, sizeof(file), "%s/%s", path, ep->d_name);
+                struct stat info;
+                stat(file, &info);
+                // directory or file
+                if (S_ISDIR(info.st_mode))
+                    RemoveDir(file);
+                else
+                    unlink(file);
+            }
+        }
+        rmdir(path);
+    }
+    
+    // close dir
+    closedir(dp);
+}
+
+// get current time
+void System::GetCurrentTime(char* time_str, int32_t len)
+{
+    time_t cur_time;
+    time(&cur_time);
+    tm*p = gmtime(&cur_time);
+    snprintf(time_str, len, "%d%02d%02d-%02d%02d%02d", (1900 + p->tm_year),
+             (1 + p->tm_mon), p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec);
+}
