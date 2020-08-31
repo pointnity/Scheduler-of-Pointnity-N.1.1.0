bdfbdaf5a8bfc19af4990b5297e266462703d1f6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-08-31 19:01:39 +0800

    Update task_action.cpp

--- src/executor/task_action.cpp
+++ src/executor/task_action.cpp
@@ -43,3 +43,4 @@ void TaskAction::TaskRunning(TaskID id) {
     //update task state
     task_ptr->SetState(TaskEntityState::TASKENTITY_RUNNING);
     if(FLAGS_debug) {
+        LOG4CPLUS_INFO(logger, "Task has running, job_id:" << id.job_id << ", task_id:" << id.task_id);
