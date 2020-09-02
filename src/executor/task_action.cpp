53c3ec78c98f451df22b4d9eaf419f0e1d98377b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-02 17:48:58 +0800

    Update task_action.cpp

--- src/executor/task_action.cpp
+++ src/executor/task_action.cpp
@@ -105,3 +105,4 @@ void TaskAction::TaskTimeout(TaskID id) {
     if (!TaskPoolI::Instance()->KillTaskByID(id)) {
             LOG4CPLUS_ERROR(logger, "Failed to kill task, job_id:" << id.job_id << ", task_id:" << id.task_id);
             return ;
+    }
