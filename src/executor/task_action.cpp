ce20ee2747b54287ab63011c052d2721d271acef
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-02 17:05:05 +0800

    Update task_action.cpp

--- src/executor/task_action.cpp
+++ src/executor/task_action.cpp
@@ -69,3 +69,4 @@ void TaskAction::TaskFinished(TaskID id) {
 void TaskAction::TaskFailed(TaskID id) {
     //delete task object
     //delete task object
+    if (!TaskPoolI::Instance()->KillTaskByID(id)) {
