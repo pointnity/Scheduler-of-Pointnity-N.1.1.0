c70236a1256aff25b0d43ec1ad982e6c043243ee
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-02 17:32:12 +0800

    Update task_action.cpp

--- src/executor/task_action.cpp
+++ src/executor/task_action.cpp
@@ -87,3 +87,4 @@ void TaskAction::TaskMissed(TaskID id) {
     //delete task object
     if (!TaskPoolI::Instance()->KillTaskByID(id)) {
             LOG4CPLUS_ERROR(logger, "Failed to kill task, job_id:" << id.job_id << ", task_id:" << id.task_id);
+            return ;
