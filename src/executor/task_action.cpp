45bb8244600c4a7321ee0f79fd28afc77804811e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-02 17:34:01 +0800

    Update task_action.cpp

--- src/executor/task_action.cpp
+++ src/executor/task_action.cpp
@@ -93,3 +93,4 @@ void TaskAction::TaskMissed(TaskID id) {
     LOG4CPLUS_INFO(logger, "Task missed, handle event of kill task action, job_id:" << id.job_id << ", task_id:" << id.task_id);
     //update task state to JM
     try {
+            Proxy<JobsManagerClient> proxy = RpcClient<JobsManagerClient>::GetProxy(FLAGS_jobs_manager_endpoint);
