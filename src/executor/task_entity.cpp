6af60e42d3dbc78106d5c88219760776096d6206
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-05 18:30:17 +0800

    Update task_entity.cpp

--- src/executor/task_entity.cpp
+++ src/executor/task_entity.cpp
@@ -149,3 +149,4 @@ bool TaskEntity::IsLegal() {
     // task, app_file
     if (!ad_ptr->EvaluateAttrString(ATTR_APP_FILE, m_info.app_info.app_file)) {
         LOG4CPLUS_ERROR(logger, "Parse " << ATTR_APP_FILE << " error.");
+        return false;
