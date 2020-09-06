a12e5b1cfa733b36705aaba19dcf8e1a8d26dc9f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-06 15:29:23 +0800

    Update task_entity.cpp

--- src/executor/task_entity.cpp
+++ src/executor/task_entity.cpp
@@ -183,3 +183,4 @@ bool TaskEntity::SetStates(const TaskEntityState::type state, const double perce
 bool TaskEntity::SetState(const TaskEntityState::type state) {
     WriteLocker locker(m_lock);
     m_state = state;
+    return true;
