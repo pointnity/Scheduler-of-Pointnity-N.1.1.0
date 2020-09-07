d8711a5f65d6668904a11735119f15a268dd43f9
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-07 14:15:10 +0800

    Update task_entity.cpp

--- src/executor/task_entity.cpp
+++ src/executor/task_entity.cpp
@@ -241,3 +241,4 @@ void TaskEntity::TaskFinished() {
         LOG4CPLUS_INFO(logger, "Task has finished, job_id:" << m_id.job_id << ", task_id:" << m_id.task_id);
     } else { 
         //kill task and vm
+        //new event
