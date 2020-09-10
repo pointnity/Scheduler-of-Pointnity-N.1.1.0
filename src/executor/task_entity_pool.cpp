c047d01243bb5e8685a26f0db1ce65c5f23e97ee
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-10 16:01:48 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -97,3 +97,4 @@ bool TaskEntityPool::StartTaskByID(const TaskID id) {
     TaskPtr ptr = GetTaskPtr(id);
     if (!ptr) {
         LOG4CPLUS_ERROR(logger, "Failed to find the task, can't start task, job_id:" << id.job_id << ", task_id:" << id.task_id);
+        return false;
