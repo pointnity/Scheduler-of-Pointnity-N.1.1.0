6cef1a501775ef4884ecaf19e9aee6150aa2d8d7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-11 19:41:47 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -34,3 +34,4 @@ void TaskEntityPool::PrintAll() {
 
 bool TaskEntityPool::Find(const TaskPtr& ptr) {
     // ptr->GetID() is TaskID(job_id, task_id)
+    map<TaskID, TaskPtr>::iterator it = m_task_map.find(ptr->GetID());
