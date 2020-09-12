0d48a959671b2e2b2d083cb40c13f590c8505c79
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-12 15:56:21 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -136,3 +136,5 @@ bool TaskEntityPool::KillTaskByID(const TaskID id) {
         LOG4CPLUS_ERROR(logger, "Failed to find the task, can't kill task, job_id:" << id.job_id << ", task_id:" << id.task_id);
         return false;
     }
+
+    // kill task
