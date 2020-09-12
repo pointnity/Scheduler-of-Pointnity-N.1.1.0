02b43dff7ade0494adb41634f642af372d0d0262
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-12 15:46:51 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -105,3 +105,4 @@ bool TaskEntityPool::StartTaskByID(const TaskID id) {
         EventPtr event(new FailedTaskEvent(id));
         // Push event into Queue
         EventDispatcherI::Instance()->Dispatch(event->GetType())->PushBack(event);
+        return false;
