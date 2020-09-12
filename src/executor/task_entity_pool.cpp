a4f83bea6a18697d641c37138eefac8080c3399f
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-12 15:59:12 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -147,3 +147,4 @@ bool TaskEntityPool::KillTaskByID(const TaskID id) {
     // Delete(id);
     // LOG4CPLUS_INFO(logger, "Kill task successfully, job_id:" << id.job_id << ", task_id:" << id.task_id);
     return true;
+}
