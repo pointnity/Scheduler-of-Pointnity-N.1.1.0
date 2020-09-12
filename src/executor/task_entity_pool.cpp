293bbec133eb9f3fc38c60022cd5d14c997bf225
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-12 15:47:18 +0800

    Update task_entity_pool.cpp

--- src/executor/task_entity_pool.cpp
+++ src/executor/task_entity_pool.cpp
@@ -106,3 +106,4 @@ bool TaskEntityPool::StartTaskByID(const TaskID id) {
         // Push event into Queue
         EventDispatcherI::Instance()->Dispatch(event->GetType())->PushBack(event);
         return false;
+    }
