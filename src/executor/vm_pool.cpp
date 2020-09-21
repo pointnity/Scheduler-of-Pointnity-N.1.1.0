8da269649797a5269134057b6eddbe95633eb960
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-21 15:01:13 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -57,3 +57,4 @@ void VMPool::PrintAll() {
     for (map<TaskID, VMPtr>::iterator it = m_vm_map.begin();
          it != m_vm_map.end(); ++it) {
         printf("VM job_id:%d, task_id:%d\n", (it->first).job_id, (it->first).task_id);
+    }
