07ab3ea29e5dc378374c97b033f6220d8202a112
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-22 10:27:02 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -121,3 +121,4 @@ int32_t VMPool::StartVM() {
     if (0 == ptr->Execute()) {
         // success start
         // LOG4CPLUS_INFO(logger, "Create environment successfully for VM, name:" << ptr->GetName() << ", job_id:" << id.job_id << ", task_id:" << id.task_id);
+    } else {
