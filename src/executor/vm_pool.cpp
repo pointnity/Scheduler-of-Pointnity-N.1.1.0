3ce61c07382e7998662bdff978f5521da479ccb4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-22 10:54:58 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -189,3 +189,4 @@ double VMPool::GetAllocatedCPU() {
     ReadLocker locker(m_lock);
     double allocated_cpu = 0;
     for (map<TaskID, VMPtr>::iterator it = m_vm_map.begin();
+        it != m_vm_map.end(); ++it) {
