a786193f208eee343a525c0cc558de07bb548b7e
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-22 10:25:26 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -111,3 +111,4 @@ int32_t VMPool::StartVM() {
     map<TaskID, VMPtr>::iterator it = m_vm_map.find(id);
     if (m_vm_map.end() == it) {
         LOG4CPLUS_ERROR(logger, "Failed to find the VM in the Pool, job_id:" << id.job_id << ", task_id:" << id.task_id);
+        LOG4CPLUS_ERROR(logger, "Can't start the VM, job_id:" << id.job_id << ", task_id:" << id.task_id); 
