652593aa388c1b1a3078a2b4339ff55add754f69
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-23 15:17:58 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -252,4 +252,6 @@ bool VMPool::ProcessHbVMInfo(const string& hb_vm_info_ad) {
         LOG4CPLUS_ERROR(logger, "VM state is false, job_id:" << id.job_id << ", task_id:" << id.task_id);
         return false;
     }
-    }
+
+
+    map<TaskID, VMPtr>::iterator it = m_vm_map.find(id);
