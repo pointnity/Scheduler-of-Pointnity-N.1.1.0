130b54100f7a0b24af3ef5d38c3ce42882590d62
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-23 14:59:50 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -225,3 +225,5 @@ bool VMPool::ProcessHbVMInfo(const string& hb_vm_info_ad) {
      // classad init, string vmhb_info --> ClassAd *ad_ptr
     ClassAdParser parser;
     ClassAd* ad_ptr = parser.ParseClassAd(hb_vm_info_ad);
+
+    if (!ad_ptr) {
