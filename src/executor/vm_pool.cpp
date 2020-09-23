e6d419a0dc2ab2ac1ba1daa7e42bef3b23cf5ef5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-23 15:12:39 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -244,3 +244,4 @@ bool VMPool::ProcessHbVMInfo(const string& hb_vm_info_ad) {
     // can't find the VM state
     if (iter == m_vm_state_map.end()) {
         LOG4CPLUS_ERROR(logger, "Failed to find the VM state , job_id:" << id.job_id << ", task_id:" << id.task_id);
+        return false;
