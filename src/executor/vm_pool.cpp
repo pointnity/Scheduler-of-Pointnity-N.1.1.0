adfe4f361137d095281e77acc734fde8a842f141
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-09-23 15:21:59 +0800

    Update vm_pool.cpp

--- src/executor/vm_pool.cpp
+++ src/executor/vm_pool.cpp
@@ -260,3 +260,4 @@ bool VMPool::ProcessHbVMInfo(const string& hb_vm_info_ad) {
         LOG4CPLUS_ERROR(logger, "Failed to find the VM, job_id:" << id.job_id << ", task_id:" << id.task_id);
         return false;
     }
+    // get VMPtr 
