e4a93f86d74055041a0569d96bf5eb1014b50676
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-11 16:34:21 +0800

    Update checker.cpp

--- src/executor_monitor/checker.cpp
+++ src/executor_monitor/checker.cpp
@@ -92,3 +92,4 @@ bool Checker::UpdateExecutorFromHdfs() {
     std::cout << "executor local path:" << executor_dir << "\nexecutor hdfs path:" << FLAGS_executor_hdfs_path <<std::endl;
     //rm old executor
     string cmd_rm = "rm -rf " + executor_dir + "/executor";
+    system(cmd_rm.c_str());
