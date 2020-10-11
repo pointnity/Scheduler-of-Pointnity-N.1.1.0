fda2e7c419895df6df5b7d4445da6beebede1eb6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-11 16:32:53 +0800

    Update checker.cpp

--- src/executor_monitor/checker.cpp
+++ src/executor_monitor/checker.cpp
@@ -86,3 +86,4 @@ bool Checker::UpdateExecutorFromHdfs() {
     char cur_dir[100];
     if (getcwd(cur_dir, sizeof(cur_dir)-1) == NULL) {
         LOG4CPLUS_ERROR(logger, "Failed to get current dir.");
+	return false;
