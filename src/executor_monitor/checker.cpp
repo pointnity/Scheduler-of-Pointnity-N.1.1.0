1418a774c0faa33b126883cbf5a78a04cf85d913
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-11 16:37:03 +0800

    Update checker.cpp

--- src/executor_monitor/checker.cpp
+++ src/executor_monitor/checker.cpp
@@ -107,3 +107,4 @@ bool Checker::UpdateExecutorFromHdfs() {
     string executor_file = executor_dir + "/executor";
     ifstream in_file(executor_file.c_str(), ios::in);
     //open file error ?
+    if(!in_file){
