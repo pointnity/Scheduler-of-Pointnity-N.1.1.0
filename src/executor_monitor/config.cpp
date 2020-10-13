a258f6f94fb7287effc1d9e376e927dae2b06513
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-13 18:18:22 +0800

    Update config.cpp

--- src/executor_monitor/config.cpp
+++ src/executor_monitor/config.cpp
@@ -10,3 +10,4 @@ DEFINE_string(resource_scheduler_endpoint, "127.0.0.1:9998", "ResouceScheduler e
 DEFINE_string(log_path, "../log/", "executor log path");
 DEFINE_string(hdfs_host, "localhost", "hdfs local host name, uname -n");
 DEFINE_int32(hdfs_port, 9000, "hdfs default port");
+DEFINE_int32(hb_interval, 5, "  ");
