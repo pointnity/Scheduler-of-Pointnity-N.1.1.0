1871e7c13826c302750e48a2eaa9cfff82af82b5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-14 12:44:52 +0800

    Update main.cpp

--- src/executor_monitor/main.cpp
+++ src/executor_monitor/main.cpp
@@ -52,3 +52,4 @@ int ExecutorMonitorEntity(int argc, char **argv) {
     SharedObjectPtr<Appender> append(new FileAppender(FLAGS_log_path + "/executor_monitor.log"));
     append->setName(LOG4CPLUS_TEXT("append for executor monitor"));
     auto_ptr<Layout> layout(new PatternLayout(LOG4CPLUS_TEXT("%d{%y/%m/%d %H:%M:%S} %p [%l]: %m %n")));
+    append->setLayout(layout);
