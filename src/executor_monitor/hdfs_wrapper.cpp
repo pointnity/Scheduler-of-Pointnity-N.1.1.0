d6e81680443a63640e75bfa19ea1a78ace9f3607
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-15 19:53:38 +0800

    Update hdfs_wrapper.cpp

--- src/executor_monitor/hdfs_wrapper.cpp
+++ src/executor_monitor/hdfs_wrapper.cpp
@@ -30,3 +30,4 @@ bool HDFSWrapper::OpenConnect() {
         LOG4CPLUS_ERROR(logger, "Failed to connect local fs");
         return false;
     }
+    m_h_fs = hdfsConnect(FLAGS_hdfs_host.c_str(), FLAGS_hdfs_port);
