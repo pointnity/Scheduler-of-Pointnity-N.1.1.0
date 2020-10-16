b81a41c874989d9532f3e04e82b1770707ad9bc2
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-16 16:54:46 +0800

    Update hdfs_wrapper.cpp

--- src/executor_monitor/hdfs_wrapper.cpp
+++ src/executor_monitor/hdfs_wrapper.cpp
@@ -67,3 +67,4 @@ bool HDFSWrapper::CloseConnect() {
     // close hdfs
     if (0 != hdfsDisconnect(m_local_fs)) {
         LOG4CPLUS_ERROR(logger, "Failed to disconnect local fs");
+        return false;
