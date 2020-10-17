1ef4a0b3586f3afd1170c9b3e4847d51d92caf77
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-17 17:31:33 +0800

    Update work_thread.cpp

--- src/executor_monitor/work_thread.cpp
+++ src/executor_monitor/work_thread.cpp
@@ -5,37 +5,3 @@
 *********************************/
 
 #include <stdlib.h>
-#include <iostream>
-#include <log4cplus/logger.h>
-#include <log4cplus/loggingmacros.h>
-#include <gflags/gflags.h>
-
-#include "include/proxy.h"
-#include "common/clynn/rpc.h"
-#include "executor_monitor/restart_executor.h"
-#include "executor_monitor/checker.h"
-
-using log4cplus::Logger;
-
-DECLARE_int32(hb_interval);
-
-static Logger logger = Logger::getInstance("executor_monitor");
-
-// thread, monitor executor
-void* ExecutorMonitorProcessor(void* unused) {
-    while(true) {
-       //executor is exist ?
-       Checker* checkerptr = new Checker();
-       if((checkerptr->IsProcess("executor")) == false) {
-	    RestartExecutor* ptr = new RestartExecutor();
-	    ptr->CheckVersionRestartExecutor();
-	    delete ptr;	
-       }
-       delete checkerptr;
-	std::cout<<"ily fisher"<<std::endl;
-        sleep(FLAGS_hb_interval);
-    }
-    return NULL;
-}
-
-#include <stdlib.h>
