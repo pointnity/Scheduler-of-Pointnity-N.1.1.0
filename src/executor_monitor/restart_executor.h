f968ecc1af35008454a4b645684e28847e2ccfa4
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-19 14:43:07 +0800

    Update restart_executor.h

--- src/executor_monitor/restart_executor.h
+++ src/executor_monitor/restart_executor.h
@@ -3,16 +3,3 @@
  Version:  0.1
  Description: restart executor
 *********************************/
-
-#ifndef SRC_EXECUTOR_MONITOR_RESTART_EXECUTOR_H
-#define SRC_EXECUTOR_MONITOR_RESTART_EXECUTOR_H
-
-#include <string>
-
-class RestartExecutor {
-public:
-    bool CheckVersionRestartExecutor();
-};
-
-
-#endif  
