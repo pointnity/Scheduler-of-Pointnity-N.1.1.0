c0fce7801c6d6caa7ea6236675dc3645ff339c93
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-10-20 12:23:08 +0800

    Update classad_attr.h

--- src/include/classad_attr.h
+++ src/include/classad_attr.h
@@ -99,3 +99,4 @@ static const string EXP_TASK_REQUIREMENT = ATTR_VCPU + " <=" + ATTR_AvailCPUNum 
 
 //image info
 static const string ATTR_IMAGE_NAME = "IMAGE_NAME";
+static const string ATTR_IMAGE_SIZE = "IMAGE_SIZE";
