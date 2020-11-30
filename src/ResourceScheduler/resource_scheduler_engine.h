6578f6a41aa187f01f4800ac8f43a9345df021ca
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-11-30 15:21:50 +0800

    Update resource_scheduler_engine.h

--- src/ResourceScheduler/resource_scheduler_engine.h
+++ src/ResourceScheduler/resource_scheduler_engine.h
@@ -25,3 +25,4 @@
 #include "common/classad/classad_complement.h"
 #include "proxy/ResourceScheduler/gen-cpp/ResourceScheduler.h"
 #include "ResourceScheduler/machine.h"
+#include "ResourceScheduler/machine_constances.h"
