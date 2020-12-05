d343ed1e8826a8846d0e9e26662bf12e24360c27
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-05 13:31:06 +0800

    Update match_maker.cpp

--- src/ResourceScheduler/match_maker.cpp
+++ src/ResourceScheduler/match_maker.cpp
@@ -23,3 +23,4 @@ int MatchMaker::Negotiation(const string& job, vector<string>& candidates) {
     candidates.clear();
     ClassAdPtr job_ad = ClassAdComplement::StringToAd(job);
     if(job_ad == NULL) {
+        LOG4CPLUS_ERROR(logger, "invalid job ad");
