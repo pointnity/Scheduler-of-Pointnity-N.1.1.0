6af3cfd4446bcbfa5bb648c4c824e00e200e7cc8
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-06 19:10:11 +0800

    Update resource_scheduler_service.cpp

--- src/ResourceScheduler/resource_scheduler_service.cpp
+++ src/ResourceScheduler/resource_scheduler_service.cpp
@@ -29,3 +29,4 @@ static Logger logger = Logger::getInstance("ResourceScheduler");
 
 void ResourceSchedulerService::GetCurVersion(string& new_version) {
     return COLLECTOR_ENGINE::Instance()->GetCurVersion(new_version);
+}
