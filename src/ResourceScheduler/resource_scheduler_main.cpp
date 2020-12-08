9752eef040220b4cb27128e2ccaf27d7bc8fd970
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-08 15:26:46 +0800

    Update resource_scheduler_main.cpp

--- src/ResourceScheduler/resource_scheduler_main.cpp
+++ src/ResourceScheduler/resource_scheduler_main.cpp
@@ -52,3 +52,4 @@ int main(int argc, char **argv){
         if(!(google::ParseCommandLineFlags(&argc, &argv, true))) {
             LOG4CPLUS_ERROR(logger, "Error happens when parsing flags from command line");
             return EXIT_FAILURE;
+        }
