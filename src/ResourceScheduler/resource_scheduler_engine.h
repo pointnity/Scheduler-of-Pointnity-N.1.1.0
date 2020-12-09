fa6b2400a4d638e5236afb193d38796d8eb4cd97
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-09 11:53:14 +0800

    Update resource_scheduler_engine.h

--- src/ResourceScheduler/resource_scheduler_engine.h
+++ src/ResourceScheduler/resource_scheduler_engine.h
@@ -63,3 +63,4 @@ class ResourceSchedulerEngine {
         bool GetIdByIp(int& id, const string& ip);
         bool MapIpToId(int& id, const string& ip);
         bool CheckAd(const ClassAd& ad);
+	bool TempAllocResource(const ClassAdPtr task, int match_machine_id, const string& machine);
