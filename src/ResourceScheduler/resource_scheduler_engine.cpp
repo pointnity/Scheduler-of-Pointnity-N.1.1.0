11e72964872dce3a9e5ac525ce67f51c4669c95d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-10 11:46:46 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -62,3 +62,4 @@ int ResourceSchedulerEngine::RegistMachine(const string& ip, const string& versi
     if(!GetIdByIp(id, ip)) {
         if(!MapIpToId(id, ip)){
             LOG4CPLUS_ERROR(logger, "The number of Machine is full");
+            return MachineError::MACHINE_ERROR_NUM_LIMIT;
