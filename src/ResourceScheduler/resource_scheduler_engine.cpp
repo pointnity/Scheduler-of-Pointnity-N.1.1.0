66d8a66263011a030f38953c0afce3973f6730bb
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-10 11:51:48 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -74,3 +74,5 @@ int ResourceSchedulerEngine::UpdateMachine(const string& machine_ad) {
     if(machine_ad_ptr == NULL) {
         return MachineError::MACHINE_ERROR_CLASSAD_SYNTAX;
     }
+
+    MachinePtr machine_ptr(new Machine(machine_ad_ptr));
