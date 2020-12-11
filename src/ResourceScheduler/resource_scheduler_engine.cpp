90f62d4164e739e9cd5cceb6e1ee5a1e1128d7d3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-11 14:41:07 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -101,3 +101,4 @@ int ResourceSchedulerEngine::NewUpdateMachine(const string& machine_ad, const ve
     //check classad 
     if(machine_ad_ptr == NULL) {
 	LOG4CPLUS_ERROR(logger, "A machine_ad is null:" << machine_ad);
+        return MachineError::MACHINE_ERROR_CLASSAD_SYNTAX;
