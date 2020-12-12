30136a35a38999eb54f0651b9caae71a3ce925cc
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-12 13:00:24 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -188,3 +188,4 @@ MultiD_Resource ResourceSchedulerEngine::GetTotalResource() {
      ReadLocker lock(m_ip_to_id_map_lock); 
      for(map<string, int>::iterator it = m_ip_to_id_map.begin(); it != m_ip_to_id_map.end(); ++it) {
          int id = it->second;
+	 ReadLocker lock(m_machine_locks[id]);
