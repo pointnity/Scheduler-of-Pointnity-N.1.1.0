39791ca839681486c6e2f08db55cf74222cacc10
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-13 11:50:25 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -258,3 +258,5 @@ int ResourceSchedulerEngine::CleanMachines(time_t now) {
 void ResourceSchedulerEngine::HousekeeperTimer(int* count, uint64_t timer_id) {
     InvokeHousekeeper();
 }
+
+bool ResourceSchedulerEngine::GetIdByIp(int& id, const string& ip) {
