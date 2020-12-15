c447ccf3d9baa1bde8d2e74a6dcfb83d0e8357b7
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-15 16:13:12 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -420,3 +420,4 @@ bool ResourceSchedulerEngine::FetchMachinesForNegTaskConst(const ClassAdPtr task
 	    match_flag = true;
 	    //for soft constraint
 	    for(vector<string>::const_iterator it = soft_list.begin(); it != soft_list.end(); ++it) {
+		 ClassAdPtr soft_constraint_ad = ClassAdComplement::StringToAd(*it);
