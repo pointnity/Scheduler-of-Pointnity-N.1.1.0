19f458ec3eab878e11f767db7c7732906328a1a3
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-15 15:56:35 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -394,3 +394,4 @@ bool ResourceSchedulerEngine::FetchMachinesForNegTaskConst(const ClassAdPtr task
 	ClassAdPtr one_machine_ptr = m_machine_pool[id]->GetMachineAd(); 
         if(CheckAd(*one_machine_ptr) == false) {
             continue;     
+        }
