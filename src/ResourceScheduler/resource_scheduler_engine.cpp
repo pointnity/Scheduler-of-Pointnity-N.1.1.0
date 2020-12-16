b45955c0bb993b2b4211d6e3d4e1d0aaa39558a6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-16 14:48:44 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -518,3 +518,4 @@ void ResourceSchedulerEngine::GetMachineListByImageInfo(vector<string>& machine_
 	    id = it->second;
             ReadLocker lock(m_machine_locks[id]);
             if(m_machine_pool[id] == NULL) {
+                continue;
