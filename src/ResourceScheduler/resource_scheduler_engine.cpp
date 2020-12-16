9c0bac671af65e89634c9fe9ab9fa8a5aba3e5e5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-16 13:53:48 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -445,3 +445,4 @@ bool ResourceSchedulerEngine::FetchMachinesForNegTaskConst(const ClassAdPtr task
 		    rank_value_max = constraint_value_all;
 		    job_rank_max = job_rank;
 		    machine = ClassAdComplement::AdTostring(one_machine_ptr);
+		    machine_endpoint = m_machine_pool[id]->GetMachineEndpoint();
