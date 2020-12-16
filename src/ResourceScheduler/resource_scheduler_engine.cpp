da80f4b163e1e7537c34edcf9577426f040b5653
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-16 13:53:18 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -443,3 +443,4 @@ bool ResourceSchedulerEngine::FetchMachinesForNegTaskConst(const ClassAdPtr task
 		is_first = false;
 	    } else if(constraint_value_all > rank_value_max) {
 		    rank_value_max = constraint_value_all;
+		    job_rank_max = job_rank;
