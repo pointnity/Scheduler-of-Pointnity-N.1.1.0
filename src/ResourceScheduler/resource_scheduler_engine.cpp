ee349d2f26bcbfeae809b6a1633f2c2f17c76459
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-16 14:03:31 +0800

    Update resource_scheduler_engine.cpp

--- src/ResourceScheduler/resource_scheduler_engine.cpp
+++ src/ResourceScheduler/resource_scheduler_engine.cpp
@@ -464,3 +464,4 @@ bool ResourceSchedulerEngine::FetchMachinesForNegTaskConst(const ClassAdPtr task
     }
 
     if(match_flag == true) {
+	if(TempAllocResource(task, match_machine_id, machine) == false) {
