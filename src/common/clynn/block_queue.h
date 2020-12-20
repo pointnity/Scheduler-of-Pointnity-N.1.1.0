22bbeaa0010681286892f067770ee4f8a1b13efd
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-20 15:59:34 +0800

    Update block_queue.h

--- src/common/clynn/block_queue.h
+++ src/common/clynn/block_queue.h
@@ -52,3 +52,4 @@ namespace clynn {
             bool PopFront(T* value, size_t timeout) {
                 MutexLocker locker(*m_mutex);
                 if (Size() <= 0) {
+                    if (m_empty->Wait(*m_mutex, timeout) == 0) {
