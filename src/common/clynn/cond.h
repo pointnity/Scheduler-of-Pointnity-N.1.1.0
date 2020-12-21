da6e1d321768aaaa2d13b8071d7ebedc14c4b1a6
Pointity Network <38712949+pointnity@users.noreply.github.com>
2020-12-21 14:55:33 +0800

    Update cond.h

--- src/common/clynn/cond.h
+++ src/common/clynn/cond.h
@@ -46,3 +46,4 @@ namespace clynn {
                 time.tv_sec = timeout;
                 time.tv_nsec = 0;
                 int ret = pthread_cond_timedwait(&m_cond, &(mutex.m_lock), &time);
+                CheckError("Cond::TimeWait", ret);
